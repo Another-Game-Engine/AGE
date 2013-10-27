ced08fd8240a0b0df8138f1536546577d1d47368
Paul Baron <paulbaron7993@gmail.com>
2013-10-27 18:42:37 +0100

    -Camera changed for the demo: 'space' to attach to earth, 'r' to reset view and 'u' for an global view

--- GameEngine/GameEngine/Demo/BasicCam.cpp
+++ GameEngine/GameEngine/Demo/BasicCam.cpp
@@ -4,7 +4,8 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "Core/Engine.hh"
 
-BasicCam::BasicCam(SmartPointer<Entity> const &toLook)
+BasicCam::BasicCam(SmartPointer<Entity> const &toLook) :
+	_lastKey(SDLK_r)
 {
 	_pos = glm::vec3(0, 5, -500);
 	_toLook = toLook;
@@ -19,15 +20,28 @@ BasicCam::~BasicCam(void)
 
 void      BasicCam::customUpdate()
 {
-	glm::vec3	lookPos = glm::vec3(_toLook->getGlobalTransform()[3].x, _toLook->getGlobalTransform()[3].y, _toLook->getGlobalTransform()[3].z);
+	glm::vec3	lookPos;
 
-	if (_pos == glm::vec3(2, 800, 0))
-		lookPos = glm::vec3(0, 0, 0);
-	if (GameEngine::instance()->inputs().getInput(SDLK_u))
+	if (_lastKey == SDLK_r)
+		lookPos = glm::vec3(_toLook->getGlobalTransform()[3].x, _toLook->getGlobalTransform()[3].y, _toLook->getGlobalTransform()[3].z);
+	else if (_lastKey == SDLK_u)
+	{
 		_pos = glm::vec3(2, 800, 0);
-	if (GameEngine::instance()->inputs().getInput(SDLK_SPACE))
+		lookPos = glm::vec3(0, 0, 0);
+	}
+	else if (_lastKey == SDLK_SPACE)
+	{
 		_pos = glm::vec3(_toLook->getGlobalTransform()[3].x, _toLook->getGlobalTransform()[3].y, _toLook->getGlobalTransform()[3].z + 50);
+		lookPos = glm::vec3(_toLook->getGlobalTransform()[3].x, _toLook->getGlobalTransform()[3].y, _toLook->getGlobalTransform()[3].z);
+	}
+	if (GameEngine::instance()->inputs().getInput(SDLK_u))
+		_lastKey = SDLK_u;
+	if (GameEngine::instance()->inputs().getInput(SDLK_SPACE))
+		_lastKey = SDLK_SPACE;
 	if (GameEngine::instance()->inputs().getInput(SDLK_r))
+	{
 		_pos = glm::vec3(0, 5, -500);
+		_lastKey = SDLK_r;
+	}
 	setTransform() = glm::lookAt(_pos, lookPos, glm::vec3(0, 1, 0));
 }
