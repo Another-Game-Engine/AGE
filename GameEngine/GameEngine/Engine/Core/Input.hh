8676bfad7d9ba672d3aa5900038acb0dcb7e220f
Paul Baron <paulbaron7993@gmail.com>
2013-10-28 22:32:52 +0100

    -Input system improved to handle key repeat

--- GameEngine/GameEngine/Engine/Core/Input.hh
+++ GameEngine/GameEngine/Engine/Core/Input.hh
@@ -10,6 +10,7 @@ class Input
 {
 private:
 	std::list<int>	_inputs;
+	std::list<int>	_keyInputs;
 	glm::i8vec2		_mousePosition;
 	glm::i8vec2		_mouseDelta;
 
@@ -19,10 +20,13 @@ public:
 
 	void 				clearInputs();
 	void 				addInput(int input);
+	void 				addKeyInput(int input);
+	void 				removeKeyInput(int input);
 	void 				setMousePosition(glm::i8vec2 const &pos);
 	glm::i8vec2 const  	&getMousePosition();
 	glm::i8vec2 const  	&getMouseDelta();
   	bool 				getInput(int input, bool handled = false);
+  	bool 				getKey(int input, bool handled = false);
 };
 
 #endif
