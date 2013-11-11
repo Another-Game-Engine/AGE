5dcfe5bdb5b499cd7211afbe667a6f637ac002b4
Cesar Leblic <cesar.leblic@gmail.com>
2013-11-11 16:51:34 +0100

    Shader class abstracted

--- GameEngine/GameEngine/Engine/OpenGL/Shader.cpp
+++ GameEngine/GameEngine/Engine/OpenGL/Shader.cpp
@@ -17,7 +17,6 @@ namespace OpenGLTools
 {
 
 Shader::Shader(void) :
-	_progId(0),
 	_vertexId(0),
 	_fragId(0),
 	_geometryId(0),
@@ -32,7 +31,6 @@ Shader::~Shader(void)
 	glDetachShader(_progId, _fragId);
 	if (_geometryId != 0)
 		glDetachShader(_progId, _geometryId);
-	glDeleteProgram(_progId);
 	if (_targets)
 		delete _targets;
 }
@@ -65,103 +63,6 @@ bool Shader::init(std::string const &vertex, std::string const &fragment, std::s
   return (true);
 }
 
-void Shader::use(void)
-{
-  glUseProgram(_progId);
-}
-
-/// link the program with the shaders
-GLuint Shader::addShader(std::string const &path, GLenum type)
-{
-  GLuint shaderId;
-  std::ifstream file(path.c_str(), std::ios_base::binary);
-  GLchar *content;
-  GLint fileSize;
-
-  if (file.fail())
-    return (0);
-  file.seekg(0, file.end);
-  fileSize = static_cast<GLint>(file.tellg()) + 1;
-  file.seekg(0, file.beg);
-  content = new GLchar[fileSize];
-  file.read(content, fileSize - 1);
-  content[fileSize - 1] = 0;
-  shaderId = glCreateShader(type);
-  glShaderSource(shaderId, 1,
-                 const_cast<const GLchar**>(&content),
-                 const_cast<const GLint*>(&fileSize));
-  compileShader(shaderId, path);
-  delete [] content;
-  return (shaderId);
-}
-
-void Shader::linkProgram(GLuint progId) const
-{
-  GLint         linkRet = 0;
-  GLsizei       msgLenght;
-  GLchar        *errorMsg;
-
-  glLinkProgram(progId);
-  glGetProgramiv(progId, GL_LINK_STATUS, &linkRet);
-  if (linkRet == GL_FALSE)
-    {
-      glGetProgramiv(progId, GL_INFO_LOG_LENGTH, &msgLenght);
-      errorMsg = new GLchar[msgLenght];
-      glGetProgramInfoLog(progId, msgLenght,
-			  &msgLenght, errorMsg);
-      std::cerr << "Link error on program : " << std::endl;
-	  std::cerr << std::string(errorMsg).data() << std::endl;
-      delete [] errorMsg;
-    }
-}
-
-void Shader::compileShader(GLuint shaderId, std::string const &file) const
-{
-  GLint         compileRet = 0;
-  GLsizei       msgLenght;
-  GLchar        *errorMsg;
-
-  glCompileShader(shaderId);
-  glGetShaderiv(shaderId, GL_COMPILE_STATUS, &compileRet);
-  if (compileRet == GL_FALSE)
-    {
-      glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &msgLenght);
-      errorMsg = new GLchar[msgLenght];
-      glGetShaderInfoLog(shaderId, msgLenght,
-			 &msgLenght, errorMsg);
-      std::cerr << "Compile error on " << file.data() << ": " << std::endl;
-      std::cerr << errorMsg << std::endl;
-      delete [] errorMsg;
-    } 
-}
-
-bool	Shader::bindUniformBlock(std::string const &blockName, UniformBuffer const &buffer)
-{
-	GLuint	blockId;
-
-	assert(_progId != 0 && "Must initialize the shader...");
-	blockId = glGetUniformBlockIndex(_progId, blockName.c_str());
-	glUniformBlockBinding(_progId, blockId, buffer.getBindingPoint());
-	return (true);
-}
-
-// function outdated -> to erase if team is agree
-Shader	&Shader::bindActiveTexture(std::string const &uniformName, GLuint activeTexture)
-{
-	use();
-	GLuint	location = glGetUniformLocation(_progId, uniformName.c_str());
-
-	glUniform1i(location, activeTexture);
-	if (glGetError() != GL_NO_ERROR)
-		std::cerr << "Bind active texture failed for uniform " << uniformName << "." << std::endl;
-	return (*this);
-}
-
-GLuint	Shader::getId() const
-{
-	return (_progId);
-}
-
 GLenum  *Shader::getTargets() const
 {
 	return _targets;
@@ -172,7 +73,7 @@ unsigned int Shader::getTargetsNumber() const
 	return _targetsList.size();
 }
 
-bool Shader::build()
+bool Shader::_build()
 {
 	use();
 
--- GameEngine/GameEngine/Engine/OpenGL/Shader.hh
+++ GameEngine/GameEngine/Engine/OpenGL/Shader.hh
@@ -1,70 +1,46 @@
-//
-// Shader.hh for  in /home/massora/GIT/amd_project/render/ShaderTool
-// 
-// Made by dorian pinaud
-// Login   <pinaud_d@epitech.net>
-// 
-// Started on  Mon Aug  5 00:33:58 2013 dorian pinaud
-// Last update Sun Aug 18 17:17:51 2013 dorian pinaud
-//
-
 #ifndef SHADER_HH_
 # define SHADER_HH_
 
-# include "Utils/OpenGL.hh"
-# include "OpenGL/UniformBuffer.hh"
-
-# include "glm/glm.hpp"
-
-# include <iostream>
 # include <map>
 # include <vector>
-# include <fstream>
-# include <string>
 # include <set>
+
+#include "AShader.hh"
 
 namespace OpenGLTools
 {
+	class Shader : public AShader
+	{
+	private:
+		GLuint						_vertexId;
+		GLuint						_fragId;
+		GLuint						_geometryId;
 
-// This class is templated on the number of uniformBuffers contained
-class Shader
-{
-private:
-	GLuint						_progId;
-	GLuint						_vertexId;
-	GLuint						_fragId;
-	GLuint						_geometryId;
-	GLenum                      *_targets;
-	GLuint                      _textureNumber;
-	std::set<GLenum>            _targetsList;
-	std::set<GLenum>      _layersList;
+		GLenum                      *_targets;
+		GLuint                      _textureNumber;
+		std::set<GLenum>            _targetsList;
+		std::set<GLenum>            _layersList;
 
-	void linkProgram(GLuint progId) const;
-	void compileShader(GLuint shaderId, std::string const &file) const;
-	GLuint addShader(std::string const &path, GLenum type);
+	public:
+		Shader(void);
+		virtual ~Shader(void);
 
-public:
-	Shader(void);
-	~Shader(void);
+		bool	init(std::string const &vertex, std::string const &fragment, std::string const &geometry = "");
 
-	bool	init(std::string const &vertex, std::string const &fragment, std::string const &geometry = "");
-	bool	bindUniformBlock(std::string const &blockName, UniformBuffer const &buff);
-	Shader	&bindActiveTexture(std::string const &uniformName, GLuint activeTexture);
+		GLenum  *getTargets() const;
+		unsigned int getTargetsNumber() const;
 
-	void	use(void);
-	GLuint	getId() const;
-	GLenum  *getTargets() const;
-	unsigned int getTargetsNumber() const;
-	bool build();
-	Shader &addTarget(GLenum target);
-	Shader &removeTarget(GLenum target);
-	void clearTargets();
-	Shader &addLayer(GLenum layer);
-	Shader &removeLayer(GLenum layer);
-	void clearLayers();
-	inline const std::set<GLenum> &getLayers() const {return _layersList;}
-	inline Shader &setTextureNumber(unsigned int t){_textureNumber = t; return *this;}
-};
+		Shader &addTarget(GLenum target);
+		Shader &removeTarget(GLenum target);
+		void clearTargets();
+		Shader &addLayer(GLenum layer);
+		Shader &removeLayer(GLenum layer);
+		void clearLayers();
+		inline const std::set<GLenum> &getLayers() const {return _layersList;}
+		inline Shader &setTextureNumber(unsigned int t){_textureNumber = t; return *this;}
+	private:
+		bool _build();
+	};
 
 }
 
