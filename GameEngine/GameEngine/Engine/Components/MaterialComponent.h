1b7d67d5998a1556fba96f8151e3595e0d014caa
Cesar Leblic <cesar.leblic@gmail.com>
2013-11-24 18:34:40 +0100

    MeshRenderSystem developed but not tested yet

---
+++ GameEngine/GameEngine/Engine/Components/MaterialComponent.h
@@ -0,0 +1,29 @@
+#ifndef  __MATERIAL_COMPONENT_H__
+#define  __MATERIAL_COMPONENT_H__
+
+#include "Components/Component.hpp"
+#include <Core/Material.hh>
+#include <Core/Engine.hh>
+#include "Utils/SmartPointer.hh"
+
+namespace Component
+{
+	class ComponentMaterial : public Component::ComponentBase<ComponentMaterial>
+	{
+	private:
+		SmartPointer<Material>             _material;
+
+		ComponentMaterial();
+		ComponentMaterial(ComponentMaterial const &);
+		ComponentMaterial                  &operator=(ComponentMaterial const &);
+	public:
+		ComponentMaterial(std::string const &name);
+		ComponentMaterial(SmartPointer<Material> material);
+		virtual                            ~ComponentMaterial(void);
+
+		bool                				setMaterial(std::string const &name);
+		SmartPointer<Material>              getMaterial() const;
+	};
+};
+
+#endif  //__MATERIAL_COMPONENT_H__
