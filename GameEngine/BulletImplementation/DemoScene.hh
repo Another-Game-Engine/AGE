fa746ccbff0eccf38c1e42bc563c262d6962e864
Cesar Leblic <cesar.leblic@gmail.com>
2013-12-19 22:20:50 +0100

    Sphere + Cube collision ok

--- GameEngine/BulletImplementation/DemoScene.hh
+++ GameEngine/BulletImplementation/DemoScene.hh
@@ -14,8 +14,9 @@ public:
 													std::string const &shader,
 													std::string const &tex1, std::string const &tex2 = "", std::string const &tex3 = "",
 													std::string const &tex4 = "");
-	Handle  DemoScene::createSphere(glm::vec3 &pos, glm::vec3 &scale, std::string const &material, std::string const &tex);
+	Handle  DemoScene::createSphere(glm::vec3 &pos, glm::vec3 &scale, std::string const &material, std::string const &tex, float mass);
 	Handle  DemoScene::createPlane(glm::vec3 &pos, glm::vec3 &scale, std::string const &material, std::string const &tex);
+	Handle  DemoScene::createCube(glm::vec3 &pos, glm::vec3 &scale, std::string const &material, std::string const &tex, float mass);
 
 	virtual bool 			userStart();
 	virtual bool 			userUpdate(double time);
