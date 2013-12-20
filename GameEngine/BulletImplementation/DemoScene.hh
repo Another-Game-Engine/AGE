fca1c07c47ae6522b8ce5f2d547ac17a8b46829b
Cesar Leblic <cesar.leblic@gmail.com>
2013-12-20 08:41:14 +0100

    Some clean before refactoring RigidBody component

--- GameEngine/BulletImplementation/DemoScene.hh
+++ GameEngine/BulletImplementation/DemoScene.hh
@@ -10,10 +10,6 @@ public:
 	DemoScene(Engine &engine);
 	virtual ~DemoScene(void);
 
-	Handle	DemoScene::createPlanet(float rotSpeed, float orbitSpeed, glm::vec3 &pos, glm::vec3 &scale,
-													std::string const &shader,
-													std::string const &tex1, std::string const &tex2 = "", std::string const &tex3 = "",
-													std::string const &tex4 = "");
 	Handle  DemoScene::createSphere(glm::vec3 &pos, glm::vec3 &scale, std::string const &material, std::string const &tex, float mass);
 	Handle  DemoScene::createPlane(glm::vec3 &pos, glm::vec3 &scale, std::string const &material, std::string const &tex);
 	Handle  DemoScene::createCube(glm::vec3 &pos, glm::vec3 &scale, std::string const &material, std::string const &tex, float mass);
