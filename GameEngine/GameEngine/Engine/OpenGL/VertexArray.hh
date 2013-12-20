cb970948d40e05a3f3e07336891c2adf7096f6ba
dorian pinaud <dorian.pinaud@epitech.eu>
2013-12-20 11:33:33 +0100

    VertexArray presque fini mais pas tester

---
+++ GameEngine/GameEngine/Engine/OpenGL/VertexArray.hh
@@ -0,0 +1,56 @@
+#ifndef VERTEXARRAY_HH_
+# define VERTEXARRAY_HH_
+
+# include <vector>
+# include <Utils/OpenGL.hh>
+
+# include "VertexBuffer.hh"
+
+namespace OpenGLTools
+{
+	/// Class representing vertexArray
+	class VertexArray
+	{
+	public:
+		struct Attribute
+		{
+			size_t nbrElement;
+			unsigned char nbrComponent;
+			unsigned char nbrByte;
+			Attribute(size_t nbrElement, unsigned char nbrComponent, unsigned char nbrByte);
+			~Attribute();
+			Attribute(Attribute const &copy);
+			Attribute &operator=(Attribute const &attribute);
+		};
+	public:
+		VertexArray();
+		~VertexArray();
+		VertexArray(VertexArray const &vertexarray);
+		VertexArray &operator=(VertexArray const &vertexarray);
+
+		void unload();
+		void addAttribute(size_t nbrElement, unsigned char nbrComponent, unsigned char nbrByte, Byte *data);
+
+		VertexBuffer const &getIndices() const;
+		void setIndices(size_t nbrElement, unsigned int *buffer);
+		void clearIndices();
+
+		VertexBuffer const &getData() const;
+		void setData(VertexBuffer const &vertexbuffer);
+		void clearData();
+
+		void transferGPU() const;
+		void handleError() const;
+
+		void draw(GLenum draw) const;
+
+	private:
+		bool _withIndex;
+		std::vector<Attribute> _attributes;
+		VertexBuffer _data;
+		VertexBuffer _indices;
+		GLuint _id;
+	};
+}
+
+#endif /*!VERTEXARRAY_HH_*/
--- GameEngine/GameEngine/Engine/OpenGL/VertexBuffer.cpp
+++ GameEngine/GameEngine/Engine/OpenGL/VertexBuffer.cpp
@@ -1,13 +1,3 @@
-//
-// VertexBuffer.cpp for  in /home/massora/GIT/amd_project/render/ShaderTool
-// 
-// Made by dorian pinaud
-// Login   <pinaud_d@epitech.net>
-// 
-// Started on  Sun Jul 28 23:59:16 2013 dorian pinaud
-// Last update Wed Aug  7 01:20:25 2013 dorian pinaud
-//
-
 #include "VertexBuffer.hh"
 
 #include <assert.h>
@@ -15,92 +5,92 @@
 namespace OpenGLTools
 {
 
-VertexBuffer::VertexBuffer(void)
-  : _idBuffer(0),
-    _idIndex(0),
-    _drawable(false)
-{
-}
+	VertexBuffer::VertexBuffer(void)
+		: _idBuffer(0),
+		_idIndex(0),
+		_drawable(false)
+	{
+	}
 
-VertexBuffer::~VertexBuffer(void)
-{
-	if (_idBuffer != 0)
-		glDeleteBuffers(1, &_idBuffer);
-	if (_idIndex != 0)
-		glDeleteBuffers(1, &_idIndex);
-}
+	VertexBuffer::~VertexBuffer(void)
+	{
+		if (_idBuffer != 0)
+			glDeleteBuffers(1, &_idBuffer);
+		if (_idIndex != 0)
+			glDeleteBuffers(1, &_idIndex);
+	}
 
-/// init the vertex buffer with information static
-void VertexBuffer::init(size_t nbrVertex, unsigned int *vertex)
-{
-  if (_idBuffer != 0)
-  	glDeleteBuffers(1, &_idBuffer);
-  if (_idIndex != 0)
-  	glDeleteBuffers(1, &_idIndex);
-  _nbrVertex = nbrVertex;
-  if (vertex)
-  {
-    glGenBuffers(2, &_idBuffer);
-    _drawable = true;
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _idIndex);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * nbrVertex, vertex, GL_DYNAMIC_DRAW);
-  }
-  else
-    glGenBuffers(1, &_idBuffer);
-}
+	/// init the vertex buffer with information static
+	void VertexBuffer::init(size_t nbrVertex, unsigned int *vertex)
+	{
+		if (_idBuffer != 0)
+			glDeleteBuffers(1, &_idBuffer);
+		if (_idIndex != 0)
+			glDeleteBuffers(1, &_idIndex);
+		_nbrVertex = nbrVertex;
+		if (vertex)
+		{
+			glGenBuffers(2, &_idBuffer);
+			_drawable = true;
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _idIndex);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)* nbrVertex, vertex, GL_DYNAMIC_DRAW);
+		}
+		else
+			glGenBuffers(1, &_idBuffer);
+	}
 
-/// Fill one blocks
-void VertexBuffer::setBuffer(size_t index, byte *buffer) const
-{
-  glBindBuffer(GL_ARRAY_BUFFER, _idBuffer);
-  glBufferSubData(GL_ARRAY_BUFFER, _attribute[index].offset, _attribute[index].nbrByte * _nbrVertex, buffer);
-}
+	/// Fill one blocks
+	void VertexBuffer::setBuffer(size_t index, byte *buffer) const
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, _idBuffer);
+		glBufferSubData(GL_ARRAY_BUFFER, _attribute[index].offset, _attribute[index].nbrByte * _nbrVertex, buffer);
+	}
 
-/// draw with the elements the contain of vbo
-void VertexBuffer::draw(GLenum mode) const
-{
-  assert(_drawable == true && "Cannot draw, buffer not initialized correctly");
-  glBindBuffer(GL_ARRAY_BUFFER, _idBuffer);
-  for (size_t i = 0; i < _attribute.size(); ++i)
-  {
-    glEnableVertexAttribArray(i);
-    glVertexAttribPointer(i, _attribute[i].nbrCompenent, _attribute[i].type, GL_FALSE, 0, (const GLvoid*)(_attribute[i].offset));
-  }
-  if (_drawable)
-  {
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _idIndex);
-    glDrawElements(mode, _nbrVertex, GL_UNSIGNED_INT, reinterpret_cast<GLvoid *>(0));
-  }
-  else
-  {
-    glDrawArrays(mode, 0, _nbrVertex);
-  }
-}
+	/// draw with the elements the contain of vbo
+	void VertexBuffer::draw(GLenum mode) const
+	{
+		assert(_drawable == true && "Cannot draw, buffer not initialized correctly");
+		glBindBuffer(GL_ARRAY_BUFFER, _idBuffer);
+		for (size_t i = 0; i < _attribute.size(); ++i)
+		{
+			glEnableVertexAttribArray(i);
+			glVertexAttribPointer(i, _attribute[i].nbrCompenent, _attribute[i].type, GL_FALSE, 0, (const GLvoid*)(_attribute[i].offset));
+		}
+		if (_drawable)
+		{
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _idIndex);
+			glDrawElements(mode, _nbrVertex, GL_UNSIGNED_INT, reinterpret_cast<GLvoid *>(0));
+		}
+		else
+		{
+			glDrawArrays(mode, 0, _nbrVertex);
+		}
+	}
 
-/// add a new attribute
-void VertexBuffer::addAttribute(Attribute const &attribute)
-{
-  Attribute r(attribute);
-  
-  if (_attribute.size() > 0)
-    {
-      r.offset = _attribute.back().offset + _attribute.back().nbrByte * _nbrVertex;
-    }
-  glBindBuffer(GL_ARRAY_BUFFER, _idBuffer);
-  size_t size = r.offset + r.nbrByte * _nbrVertex;
-  glBufferData(GL_ARRAY_BUFFER, size, 0, GL_DYNAMIC_DRAW);
-  _attribute.push_back(r);
-}
+	/// add a new attribute
+	void VertexBuffer::addAttribute(Attribute const &attribute)
+	{
+		Attribute r(attribute);
 
-/// delete a Attribute
-void VertexBuffer::clearAttributes(void)
-{
-  _attribute.clear();
-}
+		if (_attribute.size() > 0)
+		{
+			r.offset = _attribute.back().offset + _attribute.back().nbrByte * _nbrVertex;
+		}
+		glBindBuffer(GL_ARRAY_BUFFER, _idBuffer);
+		size_t size = r.offset + r.nbrByte * _nbrVertex;
+		glBufferData(GL_ARRAY_BUFFER, size, 0, GL_DYNAMIC_DRAW);
+		_attribute.push_back(r);
+	}
 
-GLuint VertexBuffer::getId() const
-{
-  return (_idBuffer);
-}
+	/// delete a Attribute
+	void VertexBuffer::clearAttributes(void)
+	{
+		_attribute.clear();
+	}
+
+	GLuint VertexBuffer::getId() const
+	{
+		return (_idBuffer);
+	}
 
 }
