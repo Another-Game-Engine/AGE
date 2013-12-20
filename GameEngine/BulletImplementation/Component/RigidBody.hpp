c8cb0973be5244f6e1c9973883c281120b799b96
Cesar Leblic <cesar.leblic@gmail.com>
2013-12-20 10:21:15 +0100

    RigidBody re implemented

--- GameEngine/BulletImplementation/Component/RigidBody.hpp
+++ GameEngine/BulletImplementation/Component/RigidBody.hpp
@@ -36,6 +36,51 @@ glm::vec3 convertBulletVectorToGLM(const btVector3& vector)
 	return glm::vec3(vector.getX(), vector.getY(), vector.getZ());
 }
 
+glm::vec3 posFromMat4(const glm::mat4 &mat)
+{
+	return glm::vec3(mat[3][0], mat[3][1], mat[3][2]);
+}
+
+glm::vec3 scaleFromMat4(const glm::mat4 &m)
+{
+	// Extract col vectors of the matrix
+	glm::vec3 col1(m[0][0], m[0][1], m[0][2]);
+	glm::vec3 col2(m[1][0], m[1][1], m[1][2]);
+	glm::vec3 col3(m[2][0], m[2][1], m[2][2]);
+	//Extract the scaling factors
+	glm::vec3 scaling;
+	scaling.x = glm::length(col1);
+	scaling.y = glm::length(col2);
+	scaling.z = glm::length(col3);
+	return scaling;
+}
+
+glm::vec3 rotFromMat4(const glm::mat4 &m, bool toEuler)
+{
+	glm::vec3 scale = scaleFromMat4(m);
+	glm::vec3 columns[3] = {
+		glm::vec3(m[0][0], m[0][1], m[0][2]),
+		glm::vec3(m[1][0], m[1][1], m[1][2]),
+		glm::vec3(m[2][0], m[2][1], m[2][2])
+	};
+	if (scale.x)
+		columns[0] /= scale.x;
+	if (scale.y)
+		columns[1] /= scale.y;
+	if (scale.z)
+		columns[2] /= scale.z;
+	glm::mat3 mat(columns[0].x, columns[1].x, columns[2].x,
+		columns[0].y, columns[1].y, columns[2].y,
+		columns[0].z, columns[1].z, columns[2].z);
+	glm::quat quat(mat);
+	glm::vec3 euler = glm::eulerAngles(quat);// ;
+	if (toEuler)
+		euler *= 3.14159f / 180.f;
+	else
+		euler *= -1;
+	return euler;
+
+}
 namespace Component
 {
 	class RigidBody : public Component::ComponentBase<RigidBody>
@@ -74,19 +119,15 @@ namespace Component
 			btTransform  mWorldTrans;
 		};
 
-		RigidBody(Engine &engine, Handle &entity)
+		RigidBody(Engine &engine, Handle &entity, float mass = 1.0f)
 			: ComponentBase(engine, entity),
 			_manager(engine.getInstance<BulletManager>()),
 			_collisionShape(nullptr),
 			_motionState(nullptr),
 			_rigidBody(nullptr),
-			_mass(1.0f),
+			_mass(mass),
 			_inertia(btVector3(0.0f, 0.0f, 0.0f))
 		{
-			_mass = btScalar(1.0f);
-			_inertia = btVector3(0.0f, 0.0f, 0.0f);
-			//_motionState = new btDefaultMotionState(convertGLMTransformToBullet(_entity.get()->getLocalTransform())); //EntityState(entity);
-			_motionState = new btDefaultMotionState(); //EntityState(entity);
 		}
 
 		btMotionState &getMotionState()
@@ -97,6 +138,11 @@ namespace Component
 		btCollisionShape &getShape()
 		{
 			return *_collisionShape;
+		}
+
+		btRigidBody &getBody()
+		{
+			return *_rigidBody;
 		}
 
 		void setMass(float mass)
@@ -113,37 +159,53 @@ namespace Component
 
 		void setCollisionShape(CollisionShape c)
 		{
+			if (_rigidBody != nullptr)
+			{
+				_manager.getWorld().removeRigidBody(_rigidBody);
+				delete _rigidBody;
+			}
+			if (_motionState != nullptr)
+			{
+				delete _motionState;
+			}
 			if (_collisionShape != nullptr)
 			{
-				if (_rigidBody != nullptr)
-				{
-					_manager.getWorld().removeRigidBody(_rigidBody);
-					delete _rigidBody;
-				}
 				delete _collisionShape;
 			}
+			btTransform transform;
+			glm::vec3 position = posFromMat4(_entity->getLocalTransform());
+			glm::vec3 scale = scaleFromMat4(_entity->getLocalTransform());
+			glm::vec3 rot = rotFromMat4(_entity->getLocalTransform(), true);
+			transform.setIdentity();
+			transform.setOrigin(convertGLMVectorToBullet(position));
+			transform.setRotation(btQuaternion(rot.x, rot.y, rot.z));
+			_motionState = new btDefaultMotionState(transform);
+			btVector3 halfScale(scale.x * 0.5f, scale.y * 0.5f, scale.z * 0.5f);
+			if (c == CUBE)
+				_collisionShape = new btBoxShape(halfScale);
+			else if (c == SPHERE)
+				_collisionShape = new btSphereShape(scale.x);
+			//// get scale
+			//glm::mat4 m = _entity->getLocalTransform();
+			//// Extract col vectors of the matrix
+			//glm::vec3 col1(m[0][0], m[0][1], m[0][2]);
+			//glm::vec3 col2(m[1][0], m[1][1], m[1][2]);
+			//glm::vec3 col3(m[2][0], m[2][1], m[2][2]);
+			////Extract the scaling factors
+			//glm::vec3 scaling;
+			//scaling.x = glm::length(col1);
+			//scaling.y = glm::length(col2);
+			//scaling.z = glm::length(col3);
 
-			// get scale
-			glm::mat4 m = _entity->getLocalTransform();
-			// Extract col vectors of the matrix
-			glm::vec3 col1(m[0][0], m[0][1], m[0][2]);
-			glm::vec3 col2(m[1][0], m[1][1], m[1][2]);
-			glm::vec3 col3(m[2][0], m[2][1], m[2][2]);
-			//Extract the scaling factors
-			glm::vec3 scaling;
-			scaling.x = glm::length(col1);
-			scaling.y = glm::length(col2);
-			scaling.z = glm::length(col3);
-
-			if (c == SPHERE)
-			{
-				_collisionShape = new btSphereShape(1.0f);
-			}
-			else if (c == CUBE)
-			{
-				_collisionShape = new btBoxShape(btVector3(0.5,0.5,0.5));
-			}
-			_collisionShape->setLocalScaling(btVector3(scaling.x, scaling.y, scaling.z));
+			//if (c == SPHERE)
+			//{
+			//	_collisionShape = new btSphereShape(1.0f);
+			//}
+			//else if (c == CUBE)
+			//{
+			//	_collisionShape = new btBoxShape(btVector3(0.5,0.5,0.5));
+			//}
+			//_collisionShape->setLocalScaling(btVector3(scaling.x, scaling.y, scaling.z));
 			_init();
 		}
 
@@ -180,18 +242,26 @@ namespace Component
 				_manager.getWorld().removeRigidBody(_rigidBody);
 				delete _rigidBody;
 			}
+
 			if (_mass != 0)
 				_collisionShape->calculateLocalInertia(_mass, _inertia);
-			btRigidBody::btRigidBodyConstructionInfo rbInfo(_mass, _motionState, _collisionShape, _inertia);
-			_rigidBody = new btRigidBody(rbInfo);
+			_rigidBody = new btRigidBody(_mass, _motionState, _collisionShape, _inertia);
+			_rigidBody->setUserPointer(&_entity);
 
-			//_rigidBody->setWorldTransform(convertGLMTransformToBullet(_entity.get()->getLocalTransform()));
-			btTransform t;
-			t = _rigidBody->getWorldTransform();
-			auto tr = _entity.get()->getLocalTransform();
-			t.setOrigin(btVector3(tr[3].x, tr[3].y, tr[3].z));
 			_manager.getWorld().addRigidBody(_rigidBody);
-			_rigidBody->setWorldTransform(t);
+
+			//if (_mass != 0)
+			//	_collisionShape->calculateLocalInertia(_mass, _inertia);
+			//btRigidBody::btRigidBodyConstructionInfo rbInfo(_mass, _motionState, _collisionShape, _inertia);
+			//_rigidBody = new btRigidBody(rbInfo);
+
+			////_rigidBody->setWorldTransform(convertGLMTransformToBullet(_entity.get()->getLocalTransform()));
+			//btTransform t;
+			//t = _rigidBody->getWorldTransform();
+			//auto tr = _entity.get()->getLocalTransform();
+			//t.setOrigin(btVector3(tr[3].x, tr[3].y, tr[3].z));
+			//_manager.getWorld().addRigidBody(_rigidBody);
+			//_rigidBody->setWorldTransform(t);
 		}
 	};
 
