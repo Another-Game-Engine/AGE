2a4448ee4d2752bd00eaa340679379ab026c3e33
Cesar Leblic <cesar.leblic@gmail.com>
2013-12-20 19:26:02 +0100

    Very basic collision component

--- GameEngine/BulletImplementation/System/BulletSystem.hpp
+++ GameEngine/BulletImplementation/System/BulletSystem.hpp
@@ -8,6 +8,7 @@
 #include <Entities/Entity.hh>
 #include "../BulletManager.hpp"
 #include <Core/Engine.hh>
+#include <Component/Collision.hpp>
 
 
 class BulletSystem : public System
@@ -22,19 +23,26 @@ private:
 	virtual void updateBegin(double time)
 	{
 		_manager.getWorld().stepSimulation(time,10);
-		//btDispatcher *dispatcher = _manager.getWorld().getDispatcher();
-		//unsigned int max = dispatcher->getNumManifolds();
-		//for (unsigned int i = 0; i < max; ++i)
-		//{
-		//	btPersistentManifold *contact = dispatcher->getManifoldByIndexInternal(i);
-		//	const btCollisionObject *oa = static_cast<const btCollisionObject*>(contact->getBody0());
-		//	const btCollisionObject *ob = static_cast<const btCollisionObject*>(contact->getBody1());
-		//	Handle h1 = *(static_cast<Handle*>(oa->getUserPointer()));
-		//	Entity *e1 = h1.get();
-		//	e1->removeComponent<Component::GraphNode>();
-		//	e1->removeComponent<Component::RigidBody>();
-		//	std::cout << "a" << std::endl;
-		//}
+		btDispatcher *dispatcher = _manager.getWorld().getDispatcher();
+		unsigned int max = dispatcher->getNumManifolds();
+		for (unsigned int i = 0; i < max; ++i)
+		{
+			btPersistentManifold *contact = dispatcher->getManifoldByIndexInternal(i);
+			const btCollisionObject *oa = static_cast<const btCollisionObject*>(contact->getBody0());
+			const btCollisionObject *ob = static_cast<const btCollisionObject*>(contact->getBody1());
+
+	/*		Handle h1 = *(static_cast<Handle*>(oa->getUserPointer()));
+			Entity *e1 = h1.get();
+			auto c1 = e1->addComponent<Component::Collision>();
+
+			Handle h2 = *(static_cast<Handle*>(ob->getUserPointer()));
+			Entity *e2 = h2.get();
+			auto c2 = e2->addComponent<Component::Collision>();
+
+
+			c1->addCollision(h2);
+			c2->addCollision(h1); */
+		}
 	}
 
 	virtual void updateEnd(double time)
@@ -57,6 +65,7 @@ private:
 			glm::vec3 scale = scaleFromMat4(e->getLocalTransform());
 			m = glm::scale(m, scale);
 			e->setLocalTransform() = m;
+			//e->removeComponent<Component::Collision>();
 		}
 	}
 
