737fa51fdf91e3872e0a7da5a0fcd71ce83fb523
Cesar Leblic <cesar.leblic@gmail.com>
2013-12-23 18:24:43 +0100

    First person camera is working

--- GameEngine/BulletImplementation/System/KinematicCharacterControllerSystem.hpp
+++ GameEngine/BulletImplementation/System/KinematicCharacterControllerSystem.hpp
@@ -64,14 +64,14 @@ private:
 			if (dir[3]) // left
 			{
 				btMatrix3x3 orn = ghost.getWorldTransform().getBasis();
-				orn *= btMatrix3x3(btQuaternion(btVector3(0, 1, 0), 0.1));
+				orn *= btMatrix3x3(btQuaternion(btVector3(0, 1, 0), -0.05));
 				ghost.getWorldTransform().setBasis(orn);
 			}
 
 			if (dir[1]) // right
 			{
 				btMatrix3x3 orn = ghost.getWorldTransform().getBasis();
-				orn *= btMatrix3x3(btQuaternion(btVector3(0, 1, 0),-0.1));
+				orn *= btMatrix3x3(btQuaternion(btVector3(0, 1, 0),0.05));
 				ghost.getWorldTransform().setBasis(orn);
 			}
 
@@ -89,13 +89,16 @@ private:
 			glm::mat4 t = convertBulletTransformToGLM(trans);
 
 
-			auto bodyPos = posFromMat4(e->getGlobalTransform());
-			Entity *prout = e.get()->getComponent<Component::GraphNode>()->getSonsBegin()->get();
-			auto cameraPos = posFromMat4(prout->getGlobalTransform());
-			auto cameraLocalPos = posFromMat4(prout->getLocalTransform());
+			//auto bodyPos = posFromMat4(e->getGlobalTransform());
+			//Entity *prout = e.get()->getComponent<Component::GraphNode>()->getSonsBegin()->get();
+			//auto cameraPos = posFromMat4(prout->getGlobalTransform());
+			//auto cameraLocalPos = posFromMat4(prout->getLocalTransform());
+			//auto cameraTrans = prout->getGlobalTransform();
+			//auto bodyTrans = e.get()->getGlobalTransform();
+			//bool YES = cameraTrans == bodyTrans;
 
 			m = glm::translate(m, posFromMat4(t));
-			glm::vec3 rot = rotFromMat4(t, false);
+			glm::vec3 rot = -rotFromMat4(t, false);
 			m = glm::rotate(m, rot.x, glm::vec3(1, 0, 0));
 			m = glm::rotate(m, rot.y, glm::vec3(0, 1, 0));
 			m = glm::rotate(m, rot.z, glm::vec3(0, 0, 1));
--- GameEngine/GameEngine/Engine/Systems/CameraSystem.hpp
+++ GameEngine/GameEngine/Engine/Systems/CameraSystem.hpp
@@ -33,6 +33,57 @@ public:
 
 protected:
 
+///
+/// TO MOVE TO UTILS
+///
+
+glm::vec3 posFromMat4(const glm::mat4 &mat)
+{
+	return glm::vec3(mat[3][0], mat[3][1], mat[3][2]);
+}
+
+glm::vec3 scaleFromMat4(const glm::mat4 &m)
+{
+	// Extract col vectors of the matrix
+	glm::vec3 col1(m[0][0], m[0][1], m[0][2]);
+	glm::vec3 col2(m[1][0], m[1][1], m[1][2]);
+	glm::vec3 col3(m[2][0], m[2][1], m[2][2]);
+	//Extract the scaling factors
+	glm::vec3 scaling;
+	scaling.x = glm::length(col1);
+	scaling.y = glm::length(col2);
+	scaling.z = glm::length(col3);
+	return scaling;
+}
+
+glm::vec3 rotFromMat4(const glm::mat4 &m, bool toEuler)
+{
+	glm::vec3 scale = scaleFromMat4(m);
+	glm::vec3 columns[3] = {
+		glm::vec3(m[0][0], m[0][1], m[0][2]),
+		glm::vec3(m[1][0], m[1][1], m[1][2]),
+		glm::vec3(m[2][0], m[2][1], m[2][2])
+	};
+	if (scale.x)
+		columns[0] /= scale.x;
+	if (scale.y)
+		columns[1] /= scale.y;
+	if (scale.z)
+		columns[2] /= scale.z;
+	glm::mat3 mat(columns[0].x, columns[1].x, columns[2].x,
+		columns[0].y, columns[1].y, columns[2].y,
+		columns[0].z, columns[1].z, columns[2].z);
+	glm::quat quat(mat);
+	glm::vec3 euler = glm::eulerAngles(quat);// ;
+	if (toEuler)
+		euler *= 3.14159f / 180.f;
+	else
+		euler *= -1;
+	return euler;
+
+}
+
+
 	std::map<SmartPointer<Material>, std::list<Handle> > _sorted;
 	bool _renderDebugMethod;
 
@@ -54,6 +105,11 @@ protected:
 		{
 			auto camera = e->getComponent<Component::CameraComponent>();
 			auto skybox = camera->getSkybox();
+			auto lookAt = e->getGlobalTransform();
+			lookAt = glm::translate(lookAt, glm::vec3(0, 0, 1));
+
+			auto cameraPosition = glm::lookAt(posFromMat4(e->getGlobalTransform()), posFromMat4(lookAt), glm::vec3(0, 1, 0));
+
 			if (skybox.get())
 			{
 				OpenGLTools::Shader *s = _engine.getInstance<Renderer>().getShader(camera->getSkyboxShader());
@@ -61,7 +117,7 @@ protected:
 
 				_engine.getInstance<Renderer>().getUniform("cameraUniform")->setUniform("projection", camera->getProjection());
 
-				glm::mat4 t = e->getGlobalTransform();
+				glm::mat4 t = cameraPosition;
 				t[3][0] = 0;
 				t[3][1] = 0;
 				t[3][2] = 0;
@@ -83,15 +139,15 @@ protected:
 			}
 
 			t += time;
+
+
+
 			// Set les uniforms du block PerFrame
 			perFrameBuffer->setUniform("projection", camera->getProjection());
-			perFrameBuffer->setUniform("view", e->getGlobalTransform());
+			perFrameBuffer->setUniform("view", cameraPosition);
 			perFrameBuffer->setUniform("time", (float)t);
 			perFrameBuffer->flushChanges();
 			_engine.getInstance<SceneManager>().getCurrentScene()->getSystem<MeshRendererSystem>()->render(time);
-			e->setLocalTransform() = glm::lookAt(glm::vec3(0,0,0),
-								 glm::vec3(0,0,1),
-								 glm::vec3(0, -1, 0));
 		}
 	}
 
