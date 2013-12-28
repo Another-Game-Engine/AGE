d785260fd32d001793775d2e364d471c501b90ae
Cesar Leblic <cesar.leblic@gmail.com>
2013-12-28 18:58:40 +0100

    TODO added in comment of SceneManager

--- GameEngine/GameEngine/Engine/Core/SceneManager.hh
+++ GameEngine/GameEngine/Engine/Core/SceneManager.hh
@@ -14,6 +14,19 @@ private:
 	SceneManager(const SceneManager &o);
 	SceneManager &operator=(const SceneManager &o);
 public:
+
+	// TO IMPLEMENT
+	// enableScene(const std::string &name, unsigned int priority);
+	// disableScene(const std::string &name);
+	// resetScene(const std::string &name);
+	// initScene(const std::string &name);
+	//
+	// Each scene should have a unique ID.
+	// Each scene should have an EntityCreator link to EntityManager
+	// EntityCreator can access EntityManager private member functions such as createEntity, deleteEntity ...
+	// EntityCreator are passed to Systems contained in scene.
+	// EntityCreator should be the unique way to create / delete ... entities
+	// When a scene is destroyed or reset the EntityCreator flush all created entities.
 	SceneManager();
 	virtual ~SceneManager();
 	void		addScene(AScene *scene, std::string const &name);
