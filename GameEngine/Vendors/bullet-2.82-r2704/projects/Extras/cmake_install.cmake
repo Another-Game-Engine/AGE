e543f58b6264ab1cf28075024a5ab1d1013337cf
Cesar Leblic <cesar.leblic@gmail.com>
2013-12-30 12:12:41 +0100

    Bullet sources added

---
+++ GameEngine/Vendors/bullet-2.82-r2704/projects/Extras/cmake_install.cmake
@@ -0,0 +1,39 @@
+# Install script for directory: C:/Users/cesar/Downloads/bullet-2.82-r2704/bullet-2.82-r2704/Extras
+
+# Set the install prefix
+IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  SET(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/BULLET_PHYSICS")
+ENDIF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+STRING(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+IF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  IF(BUILD_TYPE)
+    STRING(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  ELSE(BUILD_TYPE)
+    SET(CMAKE_INSTALL_CONFIG_NAME "Release")
+  ENDIF(BUILD_TYPE)
+  MESSAGE(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+ENDIF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+
+# Set the component getting installed.
+IF(NOT CMAKE_INSTALL_COMPONENT)
+  IF(COMPONENT)
+    MESSAGE(STATUS "Install component: \"${COMPONENT}\"")
+    SET(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  ELSE(COMPONENT)
+    SET(CMAKE_INSTALL_COMPONENT)
+  ENDIF(COMPONENT)
+ENDIF(NOT CMAKE_INSTALL_COMPONENT)
+
+IF(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  INCLUDE("C:/Users/cesar/Downloads/bullet-2.82-r2704/bullet-2.82-r2704/projects/Extras/Serialize/cmake_install.cmake")
+  INCLUDE("C:/Users/cesar/Downloads/bullet-2.82-r2704/bullet-2.82-r2704/projects/Extras/ConvexDecomposition/cmake_install.cmake")
+  INCLUDE("C:/Users/cesar/Downloads/bullet-2.82-r2704/bullet-2.82-r2704/projects/Extras/HACD/cmake_install.cmake")
+  INCLUDE("C:/Users/cesar/Downloads/bullet-2.82-r2704/bullet-2.82-r2704/projects/Extras/GIMPACTUtils/cmake_install.cmake")
+  INCLUDE("C:/Users/cesar/Downloads/bullet-2.82-r2704/bullet-2.82-r2704/projects/Extras/glui/cmake_install.cmake")
+
+ENDIF(NOT CMAKE_INSTALL_LOCAL_ONLY)
+
