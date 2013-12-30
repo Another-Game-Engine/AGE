4bf92768824f25388544333b343d87662761309f
massora <dorian.pinaud@gmail.com>
2013-12-30 18:01:02 +0100

    VertexData and Vertex are implemented

---
+++ GameEngine/GameEngine/Engine/OpenGL/Vertex.cpp
@@ -0,0 +1,61 @@
+#include "Vertex.hh"
+
+VertexData::VertexData(uint32_t nbrByte, uint32_t nbrComponent, uint32_t nbrElement, void *buffer)
+: _nbrByte(nbrByte), _nbrComponent(nbrComponent), _nbrElement(nbrElement), _sizeBuffer(nbrByte * nbrComponent * nbrElement)
+{
+	static uint32_t id = 0;
+	_id = id;
+	++id;
+	_buffer = new uint8_t[_sizeBuffer];
+	memcpy(_buffer, buffer, _sizeBuffer);
+}
+
+VertexData::VertexData(VertexData const &copy)
+: _id(copy._id), _nbrByte(copy._nbrByte), _nbrComponent(copy._nbrComponent), _nbrElement(copy._nbrElement), _sizeBuffer(copy._nbrByte * copy._nbrComponent * copy._nbrElement)
+{
+	_buffer = new uint8_t[copy._sizeBuffer];
+	memcpy(_buffer, copy._buffer, copy._sizeBuffer);
+}
+
+VertexData &VertexData::operator=(VertexData const &vertexdata)
+{
+	_id = vertexdata._id;
+	_nbrByte = vertexdata._nbrByte;
+	_nbrComponent = vertexdata._nbrComponent;
+	_nbrElement = vertexdata._nbrElement;
+	_buffer = new uint8_t[vertexdata._sizeBuffer];
+	memcpy(_buffer, vertexdata._buffer, vertexdata._sizeBuffer);
+	_sizeBuffer = vertexdata._sizeBuffer;
+	return (*this);
+}
+
+VertexData::~VertexData()
+{
+	if (_buffer)
+		delete[] _buffer;
+}
+
+uint32_t VertexData::getNbrComponent() const
+{
+	return (_nbrComponent);
+}
+
+uint32_t VertexData::getNbrByte() const
+{
+	return (_nbrByte);
+}
+
+uint32_t VertexData::getNbrElement() const
+{
+	return (_nbrElement);
+}
+
+uint32_t VertexData::getSizeBuffer() const
+{
+	return (_sizeBuffer);
+}
+
+void const * const VertexData::getBuffer() const
+{
+	return (_buffer);
+}
