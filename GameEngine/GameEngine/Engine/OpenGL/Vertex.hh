d6c91878fc1ada444775491e69c52d3c7b344cd5
massora <dorian.pinaud@gmail.com>
2014-01-01 16:36:35 +0100

    some change into the vertexManager

--- GameEngine/GameEngine/Engine/OpenGL/Vertex.hh
+++ GameEngine/GameEngine/Engine/OpenGL/Vertex.hh
@@ -37,28 +37,37 @@ public:
 	~Vertex();
 	Vertex(Vertex const &copy);
 	Vertex &operator=(Vertex const &vertex);
-	std::array<VertexData> const &getAttribute() const;
+	std::array<VertexData, NBR_ATTRIBUTE> const &getAttributes() const;
 	VertexData const * const getIndices() const;
-	bool operator==(Vertex const &vertex);
-	bool operator!=(Vertex const &vertex);
+	bool operator==(Vertex<NBR_ATTRIBUTE> const &vertex) const;
+	bool operator!=(Vertex<NBR_ATTRIBUTE> const &vertex) const;
+	uint32_t getSizeAttributes() const;
+	uint32_t getSizeIndices() const;
 private:
 	std::array<VertexData, NBR_ATTRIBUTE> _attributes;
 	VertexData *_indices;
+	uint32_t _sizeAttributes;
+	uint32_t _sizeIndices;
 };
 
 template <uint32_t NBR_ATTRIBUTE>
-Vertex<NBR_ATTRIBUTE>::Vertex(std::array<VertexData> attributes, std::array<VertexData> uniforms, VertexData const * const indices)
-: _attributes(attributes)
+Vertex<NBR_ATTRIBUTE>::Vertex(std::array<VertexData, NBR_ATTRIBUTE> attributes, VertexData const * const indices)
+: _attributes(attributes),
+	_sizeIndices(0)
 {
+	for (uint32_t index = 0; index < NBR_ATTRIBUTE; ++index)
+		_sizeAttributes += _attribute[index].getSizeBuffer();
 	if (indices)
 	{
 		_indices = new VertexData(indices);
+		_sizeIndices = _indices->getSizeBuffer();
 	}
 }
 
 template <uint32_t NBR_ATTRIBUTE>
 Vertex<NBR_ATTRIBUTE>::Vertex(Vertex<NBR_ATTRIBUTE> const &copy)
-: _attributes(copy._attributes)
+: _attributes(copy._attributes),
+_size(copy._size)
 {
 	if (copy.indices)
 	{
@@ -82,7 +91,7 @@ Vertex<NBR_ATTRIBUTE> &Vertex<NBR_ATTRIBUTE>::operator=(Vertex<NBR_ATTRIBUTE> co
 }
 
 template <uint32_t NBR_ATTRIBUTE>
-std::array<VertexData, NBR_ATTRIBUTE> const &Vertex<NBR_ATTRIBUTE>::getAttribute() const
+std::array<VertexData, NBR_ATTRIBUTE> const &Vertex<NBR_ATTRIBUTE>::getAttributes() const
 {
 	return (_attributes);
 }
@@ -94,23 +103,35 @@ VertexData const * const Vertex<NBR_ATTRIBUTE>::getIndices() const
 }
 
 template <uint32_t NBR_ATTRIBUTE>
-bool Vertex<NBR_ATTRIBUTE>::operator==(Vertex const &vertex)
+bool Vertex<NBR_ATTRIBUTE>::operator==(Vertex<NBR_ATTRIBUTE> const &vertex) const
 {
 	if (vertex._indices && _indices)
 	{
 		if (vertex._indices != _indices)
 			return (false);
 	}
-	for (size_t index = 0; index < NBR_ATTRIBUTE; ++index)
+	for (uint32_t index = 0; index < NBR_ATTRIBUTE; ++index)
 		if (_attributes[index] == vertex._attributes[index])
 			return (false);
 	return (true);
 }
 
 template <uint32_t NBR_ATTRIBUTE>
-bool Vertex<NBR_ATTRIBUTE>::operator!=(VertexData const &vertexdata)
+bool Vertex<NBR_ATTRIBUTE>::operator!=(Vertex<NBR_ATTRIBUTE> const &vertexdata) const
 {
 	return (!(*this == vertexdata));
+}
+
+template <uint32_t NBR_ATTRIBUTE>
+uint32_t Vertex<NBR_ATTRIBUTE>::getSizeAttributes() const
+{
+	return (_sizeAttributes);
+}
+
+template <uint32_t NBR_ATTRIBUTE>
+uint32_t Vertex<NBR_ATTRIBUTE>::getSizeIndices() const
+{
+	return (_sizeIndices);
 }
 
 #endif /* !VERTEX_HH_ */
--- GameEngine/GameEngine/Engine/OpenGL/VertexManager.hh
+++ GameEngine/GameEngine/Engine/OpenGL/VertexManager.hh
@@ -1,13 +1,16 @@
 #ifndef VERTEXMANAGER_HH_
 # define VERTEXMANAGER_HH_
 
+# include "Utils/OpenGL.hh"
 # include <iostream>
 # include <unordered_map>
+# include <vector>
+# include <utility>
 # include "Vertex.hh"
 # include "VertexArray.hh"
 # include "VertexBuffer.hh"
 
-template <uint16_t NBR_ATTRIBUTE, uint16_t NBR_UNIFORM>
+template <uint16_t NBR_ATTRIBUTE>
 class VertexManager
 {
 public:
@@ -15,16 +18,119 @@ public:
 	~VertexManager();
 	VertexManager(VertexManager const &copy);
 	VertexManager &operator=(VertexManager const &vertexmanager);
-	void addVertex(Vertex<NBR_ATTRIBUTE, NBR_UNIFORM> const &vertex, std::string const &name);
+	void addVertex(Vertex<NBR_ATTRIBUTE> const &vertex, std::string const &name);
 	void deleteVertex(std::string const &name);
+	void sendToGraphicProcessUnit(GLenum mode = GL_STREAM) const;
 	void clear();
-	void draw();
+	void draw(GLenum mode = GL_TRIANGLES);
 private:
+	bool _updateOnGPU;
 	OpenGLTools::VertexArray _vertexArray;
 	OpenGLTools::VertexBuffer _indexBuffer;
 	OpenGLTools::VertexBuffer _dataBuffer;
-	std::unordered_map<std::string, Vertex<NBR_ATTRIBUTE, NBR_UNIFORM> const *> _entity;
-	std::vector< Vertex<NBR_ATTRIBUTE, NBR_UNIFORM> > _pool;
+	std::unordered_map<std::string, uint32_t > _entity;
+	std::vector<t_pair<uint32_t, Vertex<NBR_ATTRIBUTE>>> _poolGPU;
+	uint32_t _sizeAttributesPoolGPU;
+	uint32_t _sizeIndicesPoolGPU;
 };
+
+template <uint16_t NBR_ATTRIBUTE>
+VertexManager<NBR_ATTRIBUTE>::VertexManager()
+: _updateOnGPU(true),
+	_sizeAttributePoolGPU(0),
+	_sizeIndicesPoolGPU(0)
+{
+}
+
+template <uint16_t NBR_ATTRIBUTE>
+VertexManager<NBR_ATTRIBUTE>::VertexManager()
+{
+}
+
+template <uint16_t NBR_ATTRIBUTE>
+VertexManager<NBR_ATTRIBUTE>::VertexManager(VertexManager<NBR_ATTRIBUTE> const &copy)
+: _updateOnGPU(copy._updateOnGPU),
+	_vertexArray(copy._vertexArray),
+	_indexBuffer(copy._indexBuffer),
+	_dataBuffer(copy._dataBuffer),
+	_entity(copy._entity),
+	_poolGPU(copy._poolGPU),
+	_sizeAttributePoolGPU(copy._sizeAttributePoolGPU),
+	_sizeIndicesPoolGPU(copy._sizeIndicesPoolGPU)
+{
+}
+
+template <uint16_t NBR_ATTRIBUTE>
+VertexManager<NBR_ATTRIBUTE> &VertexManager<NBR_ATTRIBUTE>::operator=(VertexManager<NBR_ATTRIBUTE> const &vertexmanager)
+{
+	_updateOnGPU = vertexmanager._updateOnGPU;
+	_vertexArray = vertexmanager._vertexArray;
+	_indiceBuffer = vertexmanager._indexBuffer;
+	_dataBuffer = vertexmanager._dataBuffer;
+	_entity = vertexmanager._entity;
+	_poolGPU = vertexmanager._poolGPU;
+	_sizeAttributePoolGPU = vertexmanager._sizeAttributePoolGPU;
+	_sizeIndicesPoolGPU = vertexmanager._sizeIndicesPoolGPU;
+	return (*this);
+}
+
+template <uint16_t NBR_ATTRIBUTE>
+void VertexManager<NBR_ATTRIBUTE>::addVertex(Vertex<NBR_ATTRIBUTE> const &vertex, std::string const &name)
+{
+	bool alreadyPresent = false;
+
+	for (uint32_t index = 0; index < _poolGPU.size(); ++index)
+	{
+		if (_poolGPU[index].second == vertex)
+		{
+			_poolGPU[index].first = _poolGPU[index].first + 1;
+			_entity[name] = index;
+			alreadyPresent = true;
+		}
+	}
+	if (alreadyPresent == false)
+	{
+		_updateOnGPU = true;
+		_poolGPU.push_back(std::pair<uint32_t, Vertex<NBR_ATTRIBUTE>>(1, vertex));
+		_entity[name] = _poolGPU.size() - 1;
+		_sizeAttributesPoolGPU += _poolGPU.back().getSizeAttributes();
+		_sizeIndicesPoolGPU += _poolGPU.back().getSizeIndices();
+	}
+}
+
+template <uint16_t NBR_ATTRIBUTE>
+void VertexManager<NBR_ATTRIBUTE>::deleteVertex(std::string const &name)
+{
+	if ((auto &it = _entity.find(name)) != _entity.end())
+	{
+		_poolGPU[_entity[name]].first -= 1;
+		_entity.erase(it);
+	}
+}
+
+template <uint16_t NBR_ATTRIBUTE>
+void VertexManager<NBR_ATTRIBUTE>::clear()
+{
+	for (uint32_t index = 0; index < _poolGPU.size(); ++index)
+		_poolGPU[index].first = 0;
+	_entity.clear();
+}
+
+template <uint16_t NBR_ATTRIBUTE>
+void VertexManager<NBR_ATTRIBUTE>::draw(GLenum mode)
+{
+	if (_updateOnGPU)
+		sendToGraphicProcessUnit();
+	_vertexArray.bind();
+	//draw
+	_vertexArray.unbind();
+}
+
+template <uint16_t NBR_ATTRIBUTE>
+void VertexManager<NBR_ATTRIBUTE>::sendToGraphicProcessUnit(GLenum mode) const
+{
+	_vertexArray->bind();
+	_vertexArray->unbind();
+}
 
 #endif /*!VERTEXMANAGER_HH_*/
