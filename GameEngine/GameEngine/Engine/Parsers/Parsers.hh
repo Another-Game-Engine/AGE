5ba55d2dc4a06c9f81dc09b59e1f35b1f9832534
Cesar Leblic <cesar.leblic@gmail.com>
2014-01-02 14:23:48 +0100

    Small changes on .obj loader

--- GameEngine/GameEngine/Engine/Parsers/Parsers.hh
+++ GameEngine/GameEngine/Engine/Parsers/Parsers.hh
@@ -3,11 +3,13 @@
 #define		PARSERS_HH_
 
 #include "ResourceManager/SharedMesh.hh"
+#include "tiny_obj_loader.h"
 
 GLbyte *loadTGA(const char *szFileName,
 			   GLint *iWidth, GLint *iHeight,
 			   GLint *iComponents, GLenum *eFormat);
 
 bool	loadObj(std::string const &path, std::vector<Resources::Geometry> &geometry);
+void    loadObjShape(tinyobj::shape_t &shape, Resources::Geometry &geometry);
 
 #endif
