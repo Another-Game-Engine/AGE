e1def9ab98eff354d9b18448a4269baeb1434e76
Cesar Leblic <cesar.leblic@gmail.com>
2014-01-02 14:23:48 +0100

    Small changes on .obj loader

--- GameEngine/GameEngine/Engine/Parsers/LoadOBJ.cpp
+++ GameEngine/GameEngine/Engine/Parsers/LoadOBJ.cpp
@@ -1,12 +1,9 @@
 #include "ResourceManager/SharedMesh.hh"
-
+#include "Parsers.hh"
 #include <string>
 #include <vector>
-
 #include "glm/glm.hpp"
-
 #include "Utils/File.hpp"
-#include "tiny_obj_loader.h"
 
 bool	loadObj(std::string const &path, std::vector<Resources::Geometry> &geometry)
 {
@@ -23,37 +20,43 @@ bool	loadObj(std::string const &path, std::vector<Resources::Geometry> &geometry
 	assert(err.empty());
 
 	geometry.resize(shapes.size());
-	// just on mesh for a single .obj for the moment
+
     for (size_t i = 0; i < shapes.size(); i++)
 	{
-		std::vector<glm::vec4> resPos;
-		std::vector<glm::vec4> resNorm;
-		std::vector<glm::vec2> resUv;
-		std::vector<glm::vec4> resCol;
+		loadObjShape(shapes[i], geometry[i]);
 
-		for (size_t v = 0; v < shapes[i].mesh.indices.size(); ++v)
-		{
-			unsigned int p = shapes[i].mesh.indices[v] * 3;
-			unsigned int p2 = shapes[i].mesh.indices[v] * 2;
-
-			if (shapes[i].mesh.positions.size() > 0)
-				resPos.push_back(glm::vec4(shapes[i].mesh.positions[p],
-				shapes[i].mesh.positions[p + 1],
-				shapes[i].mesh.positions[p + 2], 1));
-			if (shapes[i].mesh.normals.size() > 0)
-				resNorm.push_back(glm::vec4(shapes[i].mesh.normals[p],
-				shapes[i].mesh.normals[p + 1],
-				shapes[i].mesh.normals[p + 2], 1));
-			if (shapes[i].mesh.texcoords.size() > 0)
-				resUv.push_back(glm::vec2(shapes[i].mesh.texcoords[p2],
-				shapes[i].mesh.texcoords[p2 + 1]));
-			resCol.push_back(glm::vec4(1));
-			geometry[i].indices.push_back(v);
-		}
-		geometry[i].colors = resCol;
-		geometry[i].uvs = resUv;
-		geometry[i].normals = resNorm;
-		geometry[i].vertices = resPos;
 	}
 	return true;
+}
+
+void loadObjShape(tinyobj::shape_t &shape, Resources::Geometry &geometry)
+{
+	std::vector<glm::vec4> resPos;
+	std::vector<glm::vec4> resNorm;
+	std::vector<glm::vec2> resUv;
+	std::vector<glm::vec4> resCol;
+
+		for (size_t v = 0; v < shape.mesh.indices.size(); ++v)
+		{
+			unsigned int p = shape.mesh.indices[v] * 3;
+			unsigned int p2 = shape.mesh.indices[v] * 2;
+
+			if (shape.mesh.positions.size() > 0)
+				resPos.push_back(glm::vec4(shape.mesh.positions[p],
+				shape.mesh.positions[p + 1],
+				shape.mesh.positions[p + 2], 1));
+			if (shape.mesh.normals.size() > 0)
+				resNorm.push_back(glm::vec4(shape.mesh.normals[p],
+				shape.mesh.normals[p + 1],
+				shape.mesh.normals[p + 2], 1));
+			if (shape.mesh.texcoords.size() > 0)
+				resUv.push_back(glm::vec2(shape.mesh.texcoords[p2],
+				shape.mesh.texcoords[p2 + 1]));
+			resCol.push_back(glm::vec4(1));
+			geometry.indices.push_back(v);
+		}
+		geometry.colors = resCol;
+		geometry.uvs = resUv;
+		geometry.normals = resNorm;
+		geometry.vertices = resPos;
 }
