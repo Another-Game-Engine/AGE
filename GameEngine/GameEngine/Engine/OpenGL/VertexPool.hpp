8174b4d55221207b165bafe1a066f8c5699bdac4
massora <dorian.pinaud@gmail.com>
2014-01-07 15:33:35 +0100

    result all warning, decoup pool in another file call VertexPool, correction of VertexPool almsost finish

--- GameEngine/GameEngine/Engine/OpenGL/VertexPool.hpp
+++ GameEngine/GameEngine/Engine/OpenGL/VertexPool.hpp
@@ -1,12 +1,7 @@
 #ifdef VERTEXMANAGER_HH_
 
-/**
-* - Declaration of the struct into VertexManager.hh
-* - implement PoolElement and Pool struct use by VertexManager.hh
-*/
-
 template <uint16_t NBR_ATTRIBUTE>
-VertexManager<NBR_ATTRIBUTE>::Pool::Pool()
+VertexPool<NBR_ATTRIBUTE>::VertexPool()
 : _sizeIndices(0),
 _sizeData(0)
 {
@@ -21,12 +16,12 @@ _sizeData(0)
 }
 
 template <uint16_t NBR_ATTRIBUTE>
-VertexManager<NBR_ATTRIBUTE>::Pool::~Pool()
+VertexPool<NBR_ATTRIBUTE>::~VertexPool()
 {
 }
 
 template <uint16_t NBR_ATTRIBUTE>
-VertexManager<NBR_ATTRIBUTE>::Pool::Pool(Pool const &copy)
+VertexPool<NBR_ATTRIBUTE>::VertexPool(VertexPool const &copy)
 : _poolElements(copy._poolElements)
 _sizeIndices(copy._sizeIndices),
 _sizeData(copy._sizeData)
@@ -42,24 +37,24 @@ _sizeData(copy._sizeData)
 }
 
 template <uint16_t NBR_ATTRIBUTE>
-VertexManager<NBR_ATTRIBUTE>::Pool &VertexManager<NBR_ATTRIBUTE>::Pool::operator=(Pool const &pool)
+VertexPool<NBR_ATTRIBUTE> &VertexPool<NBR_ATTRIBUTE>::operator=(VertexPool<NBR_ATTRIBUTE> const &vertexpool)
 {
-	_poolElements = pool._poolElements;
-	_sizeIndices = pool._sizeIndices;
-	_sizeData = pool._sizeData;
+	_poolElements = vertexpool._poolElements;
+	_sizeIndices = vertexpool_sizeIndices;
+	_sizeData = vertexpool._sizeData;
 	for (uint16_t index = 0; index < NBR_ATTRIBUTE; ++index)
 	{
-		_sizeAttribute[index] = pool._sizeAttibute[index];
-		_typeAttribute[index] = pool._typeAttribute[index];
-		_normalizedAttribute[index] = pool._normalizedAttribute[index];
-		_strideAttribute[index] = pool._strideAttribute[index];
-		_pointerAttribute[index] = pool._pointerAttribute[index];
+		_sizeAttribute[index] = vertexpool._sizeAttibute[index];
+		_typeAttribute[index] = vertexpool._typeAttribute[index];
+		_normalizedAttribute[index] = vertexpool._normalizedAttribute[index];
+		_strideAttribute[index] = vertexpool._strideAttribute[index];
+		_pointerAttribute[index] = vertexpool._pointerAttribute[index];
 	}
 	return (*this)
 }
 
 template <uint16_t NBR_ATTRIBUTE>
-int32_t VertexManager<NBR_ATTRIBUTE>::Pool::operator==(Vertex<NBR_ATTRIBUTE> const &vertex) const
+int32_t VertexPool<NBR_ATTRIBUTE>::operator==(Vertex<NBR_ATTRIBUTE> const &vertex) const
 {
 	for (uint32_t index = 0; index < _pool.size(); ++index)
 		if (_poolElements[index].vertex == vertex)
@@ -68,13 +63,13 @@ int32_t VertexManager<NBR_ATTRIBUTE>::Pool::operator==(Vertex<NBR_ATTRIBUTE> con
 }
 
 template <uint16_t NBR_ATTRIBUTE>
-int32_t VertexManager<NBR_ATTRIBUTE>::Pool::operator!=(Vertex<NBR_ATTRIBUTE> const &vertex) const
+int32_t VertexPool<NBR_ATTRIBUTE>::operator!=(Vertex<NBR_ATTRIBUTE> const &vertex) const
 {
 	return (!(*this == vertex));
 }
 
 template <uint16_t NBR_ATTRIBUTE>
-bool VertexManager<NBR_ATTRIBUTE>::Pool::checkDataVertex(Vertex<NBR_ATTRIBUTE> const &vertex) const
+bool VertexPool<NBR_ATTRIBUTE>::checkDataVertex(Vertex<NBR_ATTRIBUTE> const &vertex) const
 {
 	for (uint16_t indexAtt = 0; indexAtt < NBR_ATTRIBUTE; ++indexAtt)
 	{
@@ -85,20 +80,21 @@ bool VertexManager<NBR_ATTRIBUTE>::Pool::checkDataVertex(Vertex<NBR_ATTRIBUTE> c
 }
 
 template <uint16_t NBR_ATTRIBUTE>
-int32_t &VertexManager<NBR_ATTRIBUTE>::Pool::operator+=(Vertex<NBR_ATTRIBUTE> const &vertex)
+void VertexPool<NBR_ATTRIBUTE>::setDataAttributeAtStart()
 {
-	if (_startPool == false)
-	{
-		_startPool = true;
-	}
-	else
+
+}
+
+template <uint16_t NBR_ATTRIBUTE>
+int32_t &VertexPool<NBR_ATTRIBUTE>::operator+=(Vertex<NBR_ATTRIBUTE> const &vertex)
+{
+	if (_poolElement.size() > 0)
 	{
 		if (checkDataAttribute(vertex) == false)
-		{
-			_startPool = false;
 			return (-1);
-		}
 	}
+	else
+		setDataAttributeAtStart();
 	_poolElements.push_back(1, vertex);
 	_sizeIndices += vertex.getSizeIndices();
 	_sizeData += vertex.getSizeAttributes();
@@ -106,7 +102,7 @@ int32_t &VertexManager<NBR_ATTRIBUTE>::Pool::operator+=(Vertex<NBR_ATTRIBUTE> co
 }
 
 template <uint16_t NBR_ATTRIBUTE>
-void VertexManager<NBR_ATTRIBUTE>::Pool::clear()
+void VertexPool<NBR_ATTRIBUTE>::clear()
 {
 	_poolElements.clear();
 	_sizeIndices = 0;
@@ -114,13 +110,13 @@ void VertexManager<NBR_ATTRIBUTE>::Pool::clear()
 }
 
 template <uint16_t NBR_ATTRIBUTE>
-size_t VertexManager<NBR_ATTRIBUTE>::Pool::size() const
+size_t VertexPool<NBR_ATTRIBUTE>::size() const
 {
 	return (_poolElements.size());
 }
 
 template <uint16_t NBR_ATTRIBUTE>
-GLint _VertexManager<NBR_ATTRIBUTE>::Pool::sizeAttribute(GLint index) const
+GLint VertexPool<NBR_ATTRIBUTE>::sizeAttribute(GLint index) const
 {
 
 }
