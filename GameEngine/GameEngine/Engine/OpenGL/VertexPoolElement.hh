d62507a1375f7b4d5754b40587730a78f0418b50
massora <dorian.pinaud@gmail.com>
2014-01-13 02:09:54 +0100

    vertexpoolelement finish, must finish the update vertexpool

--- GameEngine/GameEngine/Engine/OpenGL/VertexPoolElement.hh
+++ GameEngine/GameEngine/Engine/OpenGL/VertexPoolElement.hh
@@ -6,24 +6,33 @@
 # include <stdint.h>
 # include "Vertice.hh"
 
+enum AddVerticeResult
+{
+	FAIL = 0,
+	SET,
+	ADD
+};
+
 template <uint8_t NBR_ATTRIBUTE>
 class VertexPoolElement
 {
 public:
-	VertexPoolElement(uint32_t nbrEntity, Vertice<NBR_ATTRIBUTE> vertex);
+	VertexPoolElement(uint32_t nbrEntity, Vertice<NBR_ATTRIBUTE> vertices);
 	VertexPoolElement(VertexPoolElement<NBR_ATTRIBUTE> const &copy);
 	~VertexPoolElement();
 	VertexPoolElement &operator=(VertexPoolElement<NBR_ATTRIBUTE> const &element);
 
-	bool addVertice(Vertice<NBR_ATTRIBUTE> const vertice);
+	bool addVertice(Vertice<NBR_ATTRIBUTE> const &vertices);
 	void deleteVertice();
 
+	bool isEmpty() const;
 	uint32_t getVertexOffset() const;
 	uint32_t getByteOffset() const;
+	Vertice<NBR_ATTRIBUTE> const &getVertex() const;
 
 private:
 	uint32_t _nbrEntity;
-	Vertice<NBR_ATTRIBUTE> _vertex;
+	Vertice<NBR_ATTRIBUTE> _vertices;
 	uint32_t _vertexOffset;
 	uint32_t _byteOffset;
 };
