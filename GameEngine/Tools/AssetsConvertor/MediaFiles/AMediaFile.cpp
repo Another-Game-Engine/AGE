403c322ac93f7d0214513af7f494091c601e169f
Cesar Leblic <cesar.leblic@gmail.com>
2014-01-14 22:27:53 +0100

    Some old school serialization tests

--- GameEngine/Tools/AssetsConvertor/MediaFiles/AMediaFile.cpp
+++ GameEngine/Tools/AssetsConvertor/MediaFiles/AMediaFile.cpp
@@ -1,15 +1,7 @@
 #include <MediaFiles/AMediaFile.hpp>
 
-#include <cereal/archives/binary.hpp>
-#include <cereal/archives/xml.hpp>
-#include <cereal/archives/json.hpp>
-
-#include <cereal/types/polymorphic.hpp>
-
 AMediaFile::AMediaFile()
 {}
 
 AMediaFile::~AMediaFile()
 {}
-
-CEREAL_REGISTER_TYPE(AMediaFile);
