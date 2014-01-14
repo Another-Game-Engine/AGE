f26f663314e351853c42487a857093f94a3996d5
Cesar Leblic <cesar.leblic@gmail.com>
2014-01-14 22:27:53 +0100

    Some old school serialization tests

--- GameEngine/Tools/AssetsConvertor/MediaFiles/ObjFile.cpp
+++ GameEngine/Tools/AssetsConvertor/MediaFiles/ObjFile.cpp
@@ -1,15 +1,7 @@
 #include <MediaFiles/ObjFile.hpp>
 
-#include <cereal/archives/binary.hpp>
-#include <cereal/archives/xml.hpp>
-#include <cereal/archives/json.hpp>
-
-#include <cereal/types/polymorphic.hpp>
-
 ObjFile::ObjFile()
 {}
 
 ObjFile::~ObjFile()
 {}
-
-//CEREAL_REGISTER_TYPE(ObjFile);
