3dfb5cde8becb91fa3f3b01b5a864c6e1f1c17d8
Cesar Leblic <cesar.leblic@gmail.com>
2014-01-17 15:52:46 +0100

    trying to compile

--- GameEngine/Tools/AssetsConvertor/MediaFiles/AMediaFile.hpp
+++ GameEngine/Tools/AssetsConvertor/MediaFiles/AMediaFile.hpp
@@ -2,10 +2,10 @@
 # define  __AMEDIA_FILE_HPP__
 
 #include <Utils/File.hpp>
-#include <cereal/types/map.hpp>
-#include <cereal/types/vector.hpp>
-#include <cereal/types/string.hpp>
-#include <cereal/types/complex.hpp>
+//#include <cereal/types/map.hpp>
+//#include <cereal/types/vector.hpp>
+//#include <cereal/types/string.hpp>
+//#include <cereal/types/complex.hpp>
 
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
