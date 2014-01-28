379173d0a11a19748cab9e4ef79f512555b9e802
massora <dorian.pinaud@gmail.com>
2014-01-28 16:52:22 +0100

    start of shadow mapping, base project

---
+++ GameEngine/ShadowMapping/DemoScene.hh
@@ -0,0 +1,29 @@
+#ifndef DEMOSCENE_HH_
+# define DEMOSCENE_HH_
+
+# include "Core/AScene.hh"
+# include "Core/Engine.hh"
+
+#include <Systems/MeshRenderSystem.h>
+#include <Systems/GraphNodeSystem.hpp>
+#include <Systems/CameraSystem.hpp>
+#include <Systems/TrackBallSystem.hpp>
+
+#include "ResourceManager/ResourceManager.hh"
+#include "ResourceManager/Texture.hh"
+#include "ResourceManager/CubeMap.hh"
+
+class DemoScene : public AScene
+{
+public:
+	DemoScene(Engine &engine);
+	virtual ~DemoScene();
+	virtual bool userStart();
+	virtual bool userUpdate(double time);
+private:
+	void initSytemeScene();
+	void initRenderer();
+	void loadResources();
+};
+
+#endif /*!DEMOSCENE_HH_*/
