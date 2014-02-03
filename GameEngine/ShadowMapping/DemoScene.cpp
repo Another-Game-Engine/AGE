cabc568cfc1dcc6dc086fe3802aa622e10708520
dorianpinaud <dorian.pinaud@gmail.com>
2014-02-03 19:31:28 +0100

    scene ready for the shadow map

--- GameEngine/ShadowMapping/DemoScene.cpp
+++ GameEngine/ShadowMapping/DemoScene.cpp
@@ -27,7 +27,7 @@ void DemoScene::initRenderer()
 	std::string const materialBasic[] = { "ambient", "diffuse", "specular", "transmittance", "emission", "shininess" };
 
 	auto &renderer = _engine.getInstance<Renderer>();
-	OpenGLTools::Shader &s = _engine.getInstance<Renderer>().addShader("MaterialBasic", "Shaders/MaterialBasic.vp", "Shaders/MaterialBasic.fp");
+	OpenGLTools::Shader &s = renderer.addShader("MaterialBasic", "Shaders/MaterialBasic.vp", "Shaders/MaterialBasic.fp");
 	renderer.addUniform("MaterialBasic").init(&s, "MaterialBasic", materialBasic);
 	renderer.addUniform("PerFrame").init(&s, "PerFrame", perFrameVars);
 	renderer.addUniform("PerModel").init(&s, "PerModel", perModelVars);
@@ -35,15 +35,19 @@ void DemoScene::initRenderer()
 	renderer.bindShaderToUniform("MaterialBasic", "PerFrame", "PerFrame");
 	renderer.bindShaderToUniform("MaterialBasic", "PerModel", "PerModel");
 	renderer.bindShaderToUniform("MaterialBasic", "MaterialBasic", "MaterialBasic");
+	OpenGLTools::Shader &s2 = renderer.addShader("Basic", "Shaders/basic.vp", "Shaders/basic.fp");
+	renderer.bindShaderToUniform("Basic", "PerFrame", "PerFrame");
+	renderer.bindShaderToUniform("Basic", "PerModel", "PerModel");
 }
 
 void DemoScene::loadResources()
 {
 	auto &resources = _engine.getInstance<Resources::ResourceManager>();
-
+	
 	resources.addResource("model:ball", new Resources::SharedMesh(), "Assets/ball/ball.obj");
 	resources.addResource("model:cube", new Resources::SharedMesh(), "./Assets/cube/cube.obj");
 	resources.addResource("texture:earth", new Resources::Texture(), "Assets/EarthTexture.tga");
+	resources.addResource("texture:goose", new Resources::Texture(), "Assets/goose.tga");
 	resources.addResource("cubemap:space", new Resources::CubeMap(), "Assets/skyboxSpace");
 }
 
@@ -59,7 +63,8 @@ bool DemoScene::userStart()
 	Entity plan = createEntity();
 	Entity camera = createEntity();
 
-	sphere->translate(glm::vec3(0, 20, 20));
+	sphere->addComponent<Component::GraphNode>();
+	sphere->setLocalTransform() = glm::translate(sphere->getLocalTransform(), glm::vec3(0, 1, 1));
 	auto &materialSphere = sphere->addComponent<Component::MeshRenderer>("model:ball")->getMaterials()[0];
 	sphere->addComponent<Component::MeshRenderer>("model:sphere")->setShader("MaterialBasic");
 	materialSphere.ambientTex = resources.getResource("texture:earth");
@@ -67,13 +72,11 @@ bool DemoScene::userStart()
 	materialSphere.specularTex = resources.getResource("texture:earth");
 	materialSphere.normalTex = resources.getResource("texture:earth");
 	
+	plan->addComponent<Component::GraphNode>();
+	plan->setLocalTransform() = glm::translate(plan->getLocalTransform(), glm::vec3(2.5, 0, 2.5));
+	plan->setLocalTransform() = glm::scale(plan->getLocalTransform(), glm::vec3(5, 0.5, 5));
 	auto &materialPlan = plan->addComponent<Component::MeshRenderer>("model:cube")->getMaterials()[0];
 	plan->addComponent<Component::MeshRenderer>("model:cube")->setShader("MaterialBasic");
-	materialPlan.ambientTex = resources.getResource("");
-	materialPlan.diffuseTex = resources.getResource("");
-	materialPlan.specularTex = resources.getResource("");
-	materialPlan.normalTex = resources.getResource("");
-	plan->scale(glm::vec3(0, 0.1, 0));
 
 	camera->addComponent<Component::CameraComponent>();
 	auto trackBall = camera->addComponent<Component::TrackBall>(plan, 10.0f, 3.0f, 1.0f);
