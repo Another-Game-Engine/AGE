484d58257c97458a3ad1aac5bfa3ca1db031debc
Cesar Leblic <cesar.leblic@gmail.com>
2014-02-05 15:46:11 +0100

    Better initialization and destruction of audio manager

--- GameEngine/GameEngine/Engine/Audio/AudioManager.hpp
+++ GameEngine/GameEngine/Engine/Audio/AudioManager.hpp
@@ -13,7 +13,13 @@ public:
 	{}
 
 	virtual ~AudioManager()
-	{}
+	{
+		if (_system)
+		{
+			_system->release();
+			delete _system;
+		}
+	}
 
 	bool init()
 	{
@@ -51,6 +57,28 @@ public:
 		if (!fmodError(_system->getDriverCaps(0, &_caps, 0, &_speakerMode)))
 			return false;
 		if (!fmodError(_system->setSpeakerMode(_speakerMode)))
+			return false;
+
+		// CHECK IF HARDWARE ACCELERATED IS ENABLED
+		if (_caps & FMOD_CAPS_HARDWARE_EMULATED)
+		{
+			if (!fmodError(_system->setDSPBufferSize(1024, 10)))
+				return false;
+		}
+
+		// SUPPORT SIGMATEL DRIVERS
+		char name[256];
+		if (!fmodError(_system->getDriverInfo(0, name, 256, 0)))
+			return false;
+ 
+		if (std::strstr(name, "SigmaTel"))
+		{
+			if (!fmodError(_system->setSoftwareFormat(48000, FMOD_SOUND_FORMAT_PCMFLOAT, 0, 0, FMOD_DSP_RESAMPLER_LINEAR)))
+				return false;
+		}
+
+		// INITIALIZE SYSTEM
+		if (!fmodError(_system->init(32, FMOD_INIT_NORMAL, 0)))
 			return false;
 		return true;
 	}
