00ac1abb41b5fa02f5032df1dd159e4060c358ea
Cesar Leblic <cesar.leblic@gmail.com>
2014-02-05 17:28:40 +0100

    Basic sound and stream

--- GameEngine/GameEngine/Engine/Audio/FmodError.hpp
+++ GameEngine/GameEngine/Engine/Audio/FmodError.hpp
@@ -3,12 +3,4 @@
 #include <fmod_errors.h>
 #include <iostream>
 
-bool fmodError(FMOD_RESULT result)
-{
-	if (result != FMOD_OK)
-	{
-		std::cerr << "FMOD error : [" << result << "] :: " << FMOD_ErrorString(result) << std::endl;
-		return false;
-	}
-	return true;
-}
+bool fmodError(FMOD_RESULT result);
