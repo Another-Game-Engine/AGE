323665cfafd0ac15bcf8bdbef9afcc46d1822d6b
Cesar Leblic <cesar.leblic@gmail.com>
2014-02-06 14:13:56 +0100

    Listener update fixed

--- GameEngine/GameEngine/Engine/Systems/AudioSystem.hpp
+++ GameEngine/GameEngine/Engine/Systems/AudioSystem.hpp
@@ -7,6 +7,7 @@
 #include <Audio/AudioManager.hh>
 #include <Core/Engine.hh>
 #include <Utils/MatrixConversion.hpp>
+#include <glm/glm.hpp>
 
 class AudioSystem : public System
 {
@@ -45,10 +46,15 @@ protected:
 		{
 			auto l = e->getComponent<Component::AudioListener>();
 			auto pos = posFromMat4(e->getGlobalTransform());
+			glm::mat4 m = e->getGlobalTransform();
+			glm::vec4 u(0,-1,0,0);
+			u = m * u;
+			glm::vec4 f(0,0,1,0);
+			f = m * f;
+
+			FMOD_VECTOR up{ u.x, u.y, u.z };
+			FMOD_VECTOR forward{ f.x, f.y, f.z };
 			FMOD_VECTOR v{pos.x, pos.y, pos.z};
-			auto rot = rotFromMat4(e->getGlobalTransform(), false);
-			FMOD_VECTOR up{ 0, -1, 0 };
-			FMOD_VECTOR forward{ 0, 0, 1 };
 			_manager->getSystem()->set3DListenerAttributes(0, &v, 0, &forward, &up);
 		}
 	}
