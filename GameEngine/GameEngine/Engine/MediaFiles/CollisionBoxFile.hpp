6da7536fc1cd75d23c6b42e13b7bc308b415f6a1
Cesar Leblic <cesar.leblic@gmail.com>
2014-02-17 18:30:36 +0100

    File load repaired

--- GameEngine/GameEngine/Engine/MediaFiles/CollisionBoxFile.hpp
+++ GameEngine/GameEngine/Engine/MediaFiles/CollisionBoxFile.hpp
@@ -36,14 +36,16 @@ public:
 		return *this;
 	}
 
-	void unserialize(const File &f)
+	void unserialize(File f)
 	{
 		btBulletWorldImporter import(0);
 		assert(f.exists() == true && "File do not exists");
-		assert(import.loadFile(f.getFullName().c_str()) == true && "Bullet importer cannot open file.");
+		auto loadFile = import.loadFile(f.getFullName().c_str());
+		assert(loadFile == true && "Bullet importer cannot open file.");
 		int n = import.getNumCollisionShapes();
 		assert(n > 0 && "Bullet file is not correct. No collision box inside.");
-		shape = std::shared_ptr<btBoxShape>(static_cast<btBoxShape*>(import.getCollisionShapeByIndex(0)));
+		auto o = import.getCollisionShapeByIndex(0);
+		shape = std::shared_ptr<btBoxShape>(static_cast<btBoxShape*>(o));
 	}
 
 	// WILL NEVER BE CALLED
