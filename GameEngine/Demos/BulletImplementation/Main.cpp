a96b367a212905748f446140e41e1b93fd939eb6
Cesar Leblic <cesar.leblic@gmail.com>
2014-02-19 18:07:54 +0100

    Some tests

--- GameEngine/Demos/BulletImplementation/Main.cpp
+++ GameEngine/Demos/BulletImplementation/Main.cpp
@@ -54,13 +54,13 @@ int			main(int ac, char **av)
 
 	// add scene
 	e.getInstance<SceneManager>()->addScene(new BulletDemoScene(e), "BulletDemo");
-//	e.getInstance<SceneManager>()->addScene(new SolarSystemDemoScene(e), "SolarSystemDemo");
+	e.getInstance<SceneManager>()->addScene(new SolarSystemDemoScene(e), "SolarSystemDemo");
 
 	// bind scene
 	if (!e.getInstance<SceneManager>()->initScene("BulletDemo"))
 		return false;
-	//if (!e.getInstance<SceneManager>()->initScene("SolarSystemDemo"))
-	//	return false;
+	if (!e.getInstance<SceneManager>()->initScene("SolarSystemDemo"))
+		return false;
 	e.getInstance<SceneManager>()->enableScene("BulletDemo", 0);
 
 	// lanch engine
