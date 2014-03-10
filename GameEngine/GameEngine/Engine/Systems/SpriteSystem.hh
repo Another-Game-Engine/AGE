5c7bd74f04be872cba622b0eeffe861424a3d88d
Cesar Leblic <cesar.leblic@gmail.com>
2014-03-10 17:08:58 +0100

    Some tests

--- GameEngine/GameEngine/Engine/Systems/SpriteSystem.hh
+++ GameEngine/GameEngine/Engine/Systems/SpriteSystem.hh
@@ -5,6 +5,7 @@
 #include "System.h"
 #include <Components/SpriteComponent.hh>
 #include <Entities/EntityData.hh>
+#include <Context/IRenderContext.hh>
 
 class SpriteSystem : public System
 {
@@ -27,19 +28,25 @@ private:
 
 	virtual void mainUpdate(double time)
 	{
+	glm::ivec2 screen = _scene->getInstance<IRenderContext>()->getScreenSize();
+	glm::mat4 Projection = glm::mat4(1);
+	Projection *= glm::ortho(0.0f, (float)screen.x, (float)screen.y, 0.0f, -1.0f, 1.0f);
+	//auto glyphWidth = 0.0f;
+	//float lastX = position.x;
+
 		for (auto e : _filter.getCollection())
 		{
 			auto s = e->getComponent<Component::Sprite>();
 			s->shader->use();
+			auto transformationID = glGetUniformLocation(s->shader->getId(), "transformation");
+			glUniformMatrix4fv(glGetUniformLocation(s->shader->getId(), "projection"), 1, GL_FALSE, glm::value_ptr(Projection));
+			glUniform1i(glGetUniformLocation(s->shader->getId(), "fTexture0"), 0);
+			glUniform4f(glGetUniformLocation(s->shader->getId(), "color"), 1, 1, 1, 1);
 			glActiveTexture(GL_TEXTURE0);
 			glBindTexture(GL_TEXTURE_2D, s->animation->getTexture()->getId());
+			glUniformMatrix4fv(transformationID, 1, GL_FALSE, glm::value_ptr(e->getLocalTransform()));
  			s->animation->update(s->index);
 			s->animation->draw(s->index);
-			//glm::vec3 force = e->getComponent<Component::RotationForce>()->getForce();
-			//e->rotate(force * glm::vec3(t));
-			//e->setLocalTransform(glm::rotate(e->getLocalTransform(), force.x * t, glm::vec3(1, 0, 0)));
-			//e->setLocalTransform(glm::rotate(e->getLocalTransform(), force.y * t, glm::vec3(0, 1, 0)));
-			//e->setLocalTransform(glm::rotate(e->getLocalTransform(), force.z * t, glm::vec3(0, 0, 1)));
 		}
 	}
 
