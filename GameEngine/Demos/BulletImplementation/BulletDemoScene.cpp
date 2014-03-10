d6550256bc64d17da3721e34f79ba3c9449aff25
Cesar Leblic <cesar.leblic@gmail.com>
2014-03-10 13:41:44 +0100

    Json read initialized

--- GameEngine/Demos/BulletImplementation/BulletDemoScene.cpp
+++ GameEngine/Demos/BulletImplementation/BulletDemoScene.cpp
@@ -29,6 +29,7 @@
 #include <BallSoundSystem.hpp>
 
 #include <Text/FontManager.hh>
+#include <Sprite/SpriteManager.hh>
 
 #include <Core/Engine.hh>
 
@@ -207,6 +208,8 @@ bool 			BulletDemoScene::userStart()
 	getInstance<AssetsManager>()->loadFromList(File("./Assets/Serialized/export__Museum.cpd"));
 
 	getInstance<FontManager>()->loadFont(File("./Assets/Serialized/myFont.cpdFont"));
+
+	getInstance<SpriteManager>()->loadFile(File("./Assets/Serialized/GreyMan.CPDAnimation"));
 
 	_engine.getInstance<AudioManager>()->loadSound(File("./Assets/switch19.wav"), Audio::AudioSpatialType::AUDIO_3D);
 	_engine.getInstance<AudioManager>()->loadStream(File("./Assets/isolee.mp3"), Audio::AudioSpatialType::AUDIO_3D);
