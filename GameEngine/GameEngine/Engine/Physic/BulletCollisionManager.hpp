b3ba3951896df204a4ab05ffda2d2b5e96a22c56
Cesar Leblic <cesar.leblic@gmail.com>
2014-03-17 12:58:50 +0100

    First theaded tests : leak and random crash

--- GameEngine/GameEngine/Engine/Physic/BulletCollisionManager.hpp
+++ GameEngine/GameEngine/Engine/Physic/BulletCollisionManager.hpp
@@ -8,6 +8,27 @@
 #include <Entities/Entity.hh>
 #include <Entities/EntityData.hh>
 
+#include <BulletMultiThreaded/SpuGatheringCollisionDispatcher.h>
+#include <BulletMultiThreaded/PlatformDefinitions.h>
+
+#include "BulletCollision/CollisionDispatch/btSphereSphereCollisionAlgorithm.h"
+#include "BulletCollision/CollisionDispatch/btSphereTriangleCollisionAlgorithm.h"
+#include "BulletCollision/CollisionDispatch/btSimulationIslandManager.h"
+
+#include <BulletMultiThreaded/Win32ThreadSupport.h>
+#include <BulletMultiThreaded/SpuNarrowPhaseCollisionTask/SpuGatheringCollisionTask.h>
+
+#include <BulletMultiThreaded/btParallelConstraintSolver.h>
+#include <BulletMultiThreaded/SequentialThreadSupport.h>
+
+static btThreadSupportInterface *createSolverThreadSupport(int maxNumThreads)
+{
+	Win32ThreadSupport::Win32ThreadConstructionInfo threadConstructionInfo("solverThreads",SolverThreadFunc,SolverlsMemoryFunc,maxNumThreads);
+	Win32ThreadSupport* threadSupport = new Win32ThreadSupport(threadConstructionInfo);
+	threadSupport->startSPU();
+	return threadSupport;
+}
+
 class BulletCollisionManager : public Dependency
 {
 public:
@@ -19,15 +40,30 @@ public:
 
 	virtual bool init(bool init = true)
 	{
-		_dispatcher = new btCollisionDispatcher(&_defaultCollisionConfiguration);
-		_broadphase = new btDbvtBroadphase();
-		if (init) // init is false when called by Dynamic World
-		{
-			_world = new btCollisionWorld(_dispatcher, _broadphase, &_defaultCollisionConfiguration);
-			btCollisionDispatcher * dispatcher = static_cast<btCollisionDispatcher*>(_world->getDispatcher());
-			btGImpactCollisionAlgorithm::registerAlgorithm(dispatcher);
-		}
-		return true;
+		btDefaultCollisionConstructionInfo cci;
+		cci.m_defaultMaxPersistentManifoldPoolSize = 32768;
+	btCollisionConfiguration *_collisionConfiguration = new btDefaultCollisionConfiguration(cci);
+	int maxNumOutstandingTasks = 4;
+
+	SequentialThreadSupport::SequentialThreadConstructionInfo colCI("collision", processCollisionTask, createCollisionLocalStoreMemory);
+	auto m_threadSupportCollision = new SequentialThreadSupport(colCI);
+
+	_dispatcher = new	SpuGatheringCollisionDispatcher(m_threadSupportCollision, maxNumOutstandingTasks, _collisionConfiguration);
+
+	btVector3 worldAabbMin(-1000,-1000,-1000);
+	btVector3 worldAabbMax(1000,1000,1000);
+	const int maxProxies = 32766;
+	const int maxOverlap = 65535;
+	_broadphase = new btAxisSweep3(worldAabbMin, worldAabbMax, maxProxies);
+
+
+	if (init) // init is false when called by Dynamic World
+	{
+		_world = new btCollisionWorld(_dispatcher, _broadphase, _collisionConfiguration);
+		btCollisionDispatcher * dispatcher = static_cast<btCollisionDispatcher*>(_world->getDispatcher());
+		btGImpactCollisionAlgorithm::registerAlgorithm(dispatcher);
+	}
+	return true;
 	}
 
 	virtual ~BulletCollisionManager()
@@ -77,7 +113,7 @@ protected:
 	btCollisionWorld *_world;
 	btCollisionDispatcher *_dispatcher;
 	btBroadphaseInterface *_broadphase;
-	btDefaultCollisionConfiguration _defaultCollisionConfiguration;
+	btCollisionConfiguration *_collisionConfiguration;
 };
 
 #endif    //__BULLET_COLLISION_MANAGER_HPP__
--- GameEngine/GameEngine/Engine/Physic/BulletDynamicManager.hpp
+++ GameEngine/GameEngine/Engine/Physic/BulletDynamicManager.hpp
@@ -22,6 +22,26 @@ public:
 	{
 		if (!BulletCollisionManager::init(false))
 			return false;
+
+		btThreadSupportInterface *m_threadSupportSolver = nullptr;
+		int maxNumOutstandingTasks = 4;
+
+	m_threadSupportSolver = createSolverThreadSupport(maxNumOutstandingTasks);
+	btConstraintSolver *m_solver = new btParallelConstraintSolver(m_threadSupportSolver);
+	//this solver requires the contacts to be in a contiguous pool, so avoid dynamic allocation
+	_dispatcher->setDispatcherFlags(btCollisionDispatcher::CD_DISABLE_CONTACTPOOL_DYNAMIC_ALLOCATION);
+
+
+	btDiscreteDynamicsWorld* world = new btDiscreteDynamicsWorld(_dispatcher, _broadphase, m_solver, _collisionConfiguration);
+
+	world->getSimulationIslandManager()->setSplitIslands(false);
+	world->getSolverInfo().m_numIterations = 4;
+	world->getSolverInfo().m_solverMode = SOLVER_SIMD + SOLVER_USE_WARMSTARTING;//+SOLVER_RANDMIZE_ORDER;
+
+	world->getDispatchInfo().m_enableSPU = true;
+	world->setGravity(btVector3(0, -10, 0));
+
+
 		_constraintSolver = new btSequentialImpulseConstraintSolver();
 		// DISGUSTING !! LEAK !!!
 		btVector3 worldMin(-1000, -1000, -1000);
@@ -29,7 +49,7 @@ public:
 		btAxisSweep3* sweepBP = new btAxisSweep3(worldMin, worldMax);
 		static auto test = sweepBP;
 
-		_world = new btDiscreteDynamicsWorld(_dispatcher, test, _constraintSolver, &_defaultCollisionConfiguration);
+		_world = world;
 //		_world->getDispatchInfo().m_allowedCcdPenetration = 0.01f;
 		static_cast<btDiscreteDynamicsWorld *>(_world)->setGravity(btVector3(0, -10, 0));
 		return true;
