f620efbbd3dd98949d68e3e113a6137a17573945
Cesar Leblic <cesar.leblic@gmail.com>
2014-03-19 14:43:33 +0100

    FPSsystem improved

--- GameEngine/Demos/DemoSceneV0/Scenes/MainScene.cpp
+++ GameEngine/Demos/DemoSceneV0/Scenes/MainScene.cpp
@@ -3,6 +3,9 @@
 //systems
 #include <SharedSystems/FPSSystem.hh>
 
+// SDL
+#include <Context/SdlContext.hh>
+
 MainScene::MainScene(std::weak_ptr<Engine> engine)
 : AScene(engine)
 {}
@@ -22,5 +25,16 @@ bool 			MainScene::userStart()
 
 bool 			MainScene::userUpdate(double time)
 {
+	if (getInstance<Input>()->getInput(SDLK_ESCAPE) ||
+		getInstance<Input>()->getInput(SDL_QUIT))
+	{
+		//SERIALIZATION
+		{
+			std::ofstream s("BulletScene.scenesave", std::ios_base::binary);
+			save<cereal::BinaryOutputArchive>(s);
+			s.close();
+		}
+		return false;
+	}
 	return true;
 }
