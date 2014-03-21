8b8832282450ae897d2d6b3768f6e2fc9e0fc22d
Cesar Leblic <cesar.leblic@gmail.com>
2014-03-21 19:25:49 +0100

    default white texture for materials

--- GameEngine/Demos/DemoSceneV0/Rooms/EngineRoom.hh
+++ GameEngine/Demos/DemoSceneV0/Rooms/EngineRoom.hh
@@ -7,6 +7,8 @@ struct EngineRoom : public Room
 	EngineRoom(std::weak_ptr<AScene> scene);
 	virtual ~EngineRoom();
 	virtual bool init();
+	Entity cat;
+	Entity house;
 protected:
 	virtual bool _enable();
 	virtual bool _disable();
