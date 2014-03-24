d156ec6f757368b08672f6d3473cca5ae3a3b2c0
César Leblic <cesar.leblic@gmail.com>
2014-03-24 14:22:56 +0100

    Main room initialized in MainScene

--- GameEngine/Demos/DemoSceneV0/Rooms/Entrance.cpp
+++ GameEngine/Demos/DemoSceneV0/Rooms/Entrance.cpp
@@ -21,48 +21,12 @@
 	{
 		// load
 		auto s = _scene.lock();
-		s->getInstance<AssetsManager>()->loadFromList(File("../../Assets/Serialized/export__demoMuseum.cpd"));
 		s->getInstance<SpriteManager>()->loadFile(File("../../Assets/Serialized/Pong.CPDAnimation"));
 		s->getInstance<SpriteManager>()->loadFile(File("../../Assets/Serialized/Trololo.CPDAnimation"));
 		s->getInstance<SpriteManager>()->loadFile(File("../../Assets/Serialized/SceneReader.CPDAnimation"));
 		s->getInstance<AssetsManager>()->loadFromList(File("../../Assets/Serialized/export__3DTexts.cpd"));
 
-		auto scene = _scene.lock();
-		{
-			room = scene->createEntity();
-			room->setLocalTransform(glm::translate(room->getLocalTransform(), glm::vec3(0)));
-			room->setLocalTransform(glm::scale(room->getLocalTransform(), glm::vec3(150.0f)));
-			auto meshObj = scene->getInstance<AssetsManager>()->get<ObjFile>("obj__demoMuseum");
-			if (!meshObj)
-				return false;
-			auto meshComponent = room->addComponent<Component::MeshRenderer>(meshObj);
-			meshComponent->setShader("MaterialBasic");
-			auto rigidBody = room->addComponent<Component::RigidBody>(0.0f);
-			rigidBody->setMass(0);
-			rigidBody->setCollisionShape(Component::RigidBody::MESH, "collision_shape_static_demoMuseum");
-		//	rigidBody->getShape().setMargin(0.001f);
-			rigidBody->getBody().setFriction(1.0f);
-			rigidBody->getBody().setRestitution(0.9f);
-		}
 		hotZone = createHotZone("Entrance->Projection", "HZ-entrance-projection");
-
-
-		////////
-		////
-		/// FBO id receiver
-		{
-			//auto e = scene->createEntity();
-			//e->setLocalTransform(glm::translate(e->getLocalTransform(), glm::vec3(-8, 1, 0)));
-			//auto sprite = e->addComponent<Component::Sprite>(scene->getInstance<SpriteManager>()->getAnimation("shoot", "shoot"));
-			//sprite->delay = 1.0f / 10.0f;
-			//e->addComponent<Component::TransformationRegister>("test-fb-tableau");
-			//e->addComponent<Component::EntityPlacable>("test-fb-tableau");
-			//fboTest = e;
-			//auto &_id = sprite->animation->getMaterial().ambientTex->id;
-			//framebufferIdReceiver.globalSub(PubSubKey("myTextureKeyIs"), [&_id](GLuint id) {
-			//	_id = id;
-			//});
-		}
 
 		return true;
 	}
