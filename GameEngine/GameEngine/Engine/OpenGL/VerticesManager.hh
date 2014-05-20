e2f91e5c2752fc0e07f168d21924260e7ee7f7c1
dorianpinaud <dorian.pinaud@gmail.com>
2014-05-20 23:31:36 +0200

    refond file archi

---
+++ GameEngine/GameEngine/Engine/OpenGL/VerticesManager.hh
@@ -0,0 +1,52 @@
+#ifndef VERTICESMANAGER_HH_
+# define VERTICESMANAGER_HH_
+
+#include <Utils/OpenGL.hh>
+#include <OpenGL/Key.hh>
+#include <OpenGL/VertexBuffer.hh>
+#include <Utils/Dependency.hpp>
+#include <vector>
+#include <utility>
+
+namespace gl
+{
+	class Vertices;
+	class VerticesPool;
+
+	//!\file VerticesManager.hh
+	//!\author Dorian Pinaud
+	//!\version v1.0
+	//!\class VerticesManager
+	//!\brief Handle the geometry of the render
+	class VerticesManager : public Dependency
+	{
+	public:
+		// constructor
+		VerticesManager();
+		~VerticesManager();
+
+		// handle pools
+		Key<VerticesPool> const &addPool();
+		Key<VerticesPool> const &addPool(uint8_t nbrAttributes, GLenum *typeComponent, uint8_t *sizeTypeComponent, uint8_t *nbrComponent);
+		Key<VerticesPool> getPool(size_t index) const;
+		size_t nbrPool() const;
+		VerticesManager &rmPool(Key<VerticesPool> const &key);
+
+		//handle Vertices
+		Key<Vertices> const &addVertices(size_t nbrVertices, uint8_t nbrBuffers, size_t *sizeBuffers, void **buffers);
+		VerticesManager &rmVertices(Key<Vertices> const &key);
+		Key<Vertices> getVertices(size_t index) const;
+		size_t getNbrVertices() const;
+
+		// attach vertices to pools
+		VerticesManager &attachVerticesToPool(Key<Vertices> const &vertices, Key<VerticesPool> const &pool);
+		VerticesManager &detachVerticesToPool(Key<Vertices> const &vertices, Key<VerticesPool> const &pool);
+
+	private:
+		// data represent pools
+		std::vector<std::pair<Key<VerticesPool>, VerticesPool>> _pools;
+		std::vector<std::pair<Key<Vertices>, Vertices>> _vertices;
+	};
+}
+
+#endif
