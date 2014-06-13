7883915121373fe504a08d2b79f1bbf15879d3d3
dorianpinaud <dorian.pinaud@gmail.com>
2014-06-13 22:01:26 +0200

    base of class uniform done

---
+++ GameEngine/GameEngine/Engine/OpenGL/Uniform.cpp
@@ -0,0 +1,43 @@
+#include <OpenGL/Uniform.hh>
+#include <stdint.h>
+
+namespace gl
+{
+	Uniform::Uniform(std::string const &flag)
+		: _flag(flag),
+		_data(NULL),
+		_sizeData(0)
+	{
+
+	}
+
+	Uniform::Uniform(Uniform const &uniform)
+		: Uniform(uniform._flag)
+	{
+		_sizeData = uniform._sizeData;
+		_data = new uint8_t[_sizeData];
+		memcpy(_data, uniform._data, _sizeData);
+	}
+
+	Uniform &Uniform::operator=(Uniform const &u)
+	{
+		if (this != &u)
+		{
+			if (u._sizeData != _sizeData)
+			{
+				_sizeData = u._sizeData;
+				if (_data)
+					delete _data;
+				_data = new uint8_t[_sizeData];
+			}
+			memcpy(_data, u._data, _sizeData);
+		}
+		return (*this);
+	}
+
+	Uniform::~Uniform()
+	{
+		if (_data)
+			delete _data;
+	}
+}
---
+++ GameEngine/GameEngine/Engine/OpenGL/Uniform.hh
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <string>
+
+namespace gl
+{
+	//!\file Uniform.hh
+	//!\author Dorian Pinaud
+	//!\version v1.0
+	//!\class Uniform
+	//!\brief Handle the uniform using in shader
+	class Uniform
+	{
+	public:
+		Uniform(std::string const &flag);
+		~Uniform();
+		template <typename TYPE> Uniform(std::string const &flag, TYPE const &data);
+		Uniform(Uniform const &uniform);
+		Uniform &operator=(Uniform const &uniform);
+		template <typename TYPE> TYPE getData() const;
+		template <typename TYPE> Uniform &setData(TYPE const &data);
+		template <typename TYPE> Uniform &syncronisation();
+	private:
+		std::string const _flag;
+		void *_data;
+		size_t _sizeData;
+	};
+
+# define DEBUG_MESSAGE(type, from, reason, return_type) \
+	{	std::cerr << std::string(type) + ": from[" + std::string(from) + "], reason[" + std::string(reason) + "]." << std::endl; return return_type; }
+
+	template <typename TYPE>
+	Uniform::Uniform(std::string const &flag, TYPE const &data)
+		: Uniform(flag)
+	{
+		_data = new TYPE;
+		_sizeData = sizeof(TYPE);
+		memcpy(_data, &data, _sizeData);
+	}
+
+	template <typename TYPE>
+	TYPE Uniform::getData() const
+	{
+		TYPE value;
+
+		if (sizeof(TYPE) != _sizeData)
+			DEBUG_MESSAGE("Error", "Uniform.hh - getData()", "The size of value use for the cast is different of the value intern", value);
+		memcpy(&value, _data, _sizeData);
+		return (value);
+	}
+
+	template <typename TYPE>
+	Uniform &Uniform::setData(TYPE const &value)
+	{
+		if (sizeof(TYPE) != _sizeData)
+			DEBUG_MESSAGE("Error", "Uniform.hh - getData()", "The size of value use for the cast is different of the value intern", *this);
+		memcpy(_data, &value, _sizeData);
+		return (*this);
+	}
+
+	// code all syncronisation func you need
+}
