d00ccbe34eee4c52a85d0e23651ba3fd5ef78f7e
Cesar Leblic <cesar.leblic@gmail.com>
2014-06-22 18:35:43 +0200

    Animation loader

--- GameEngine/Tools/AssetsEditor/Main.cpp
+++ GameEngine/Tools/AssetsEditor/Main.cpp
@@ -1,4 +1,5 @@
 #include <Convertor/SkeletonLoader.hpp>
+#include <Convertor/AnimationsLoader.hpp>
 
 int main(void)
 {
@@ -6,5 +7,6 @@ int main(void)
 	dataSet.filePath = File("../../Assets/catwoman/atk close front 6.fbx");
 	dataSet.name = "catwoman";
 	auto skeleton = AGE::SkeletonLoader::load(dataSet);
+	auto animations = AGE::AnimationsLoader::load(dataSet);
 	return 0;
 }
