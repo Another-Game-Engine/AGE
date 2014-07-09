5318315784c5a6eba7b950801f4575039e4c5a7f
Cesar Leblic <cesar.leblic@gmail.com>
2014-07-09 13:32:20 +0200

    Plane class

---
+++ GameEngine/GameEngine/Engine/Utils/Plane.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace AGE
+{
+	class Plane
+	{
+	public:
+		Plane(const glm::vec3 &normal = glm::vec3(0,0,0), float distance = 0.0f)
+		{}
+
+		void normalize()
+		{
+			float divider = glm::length(_normal);
+			_normal /= divider;
+			_distance /= divider;
+		}
+
+		const glm::vec3 &getNormal() const { return _normal; }
+		float getDistance() const { return _distance; }
+		void setNormal(const glm::vec3 normal) { _normal = normal; }
+		void setDistance(float distance) { _distance = distance; }
+	private:
+		glm::vec3 _normal;
+		float _distance;
+	};
+}
