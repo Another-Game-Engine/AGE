73639d1b4f8bacdde46c88fddc24a62d2e8e7f2b
dorianpinaud <dorian.pinaud@gmail.com>
2014-07-12 18:55:15 +0200

    rethink renderPass

--- GameEngine/GameEngine/Engine/OpenGL/RenderPass.cpp
+++ GameEngine/GameEngine/Engine/OpenGL/RenderPass.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <string>
 #include <OpenGL/Shader.hh>
+#include <OpenGL/Drawable.hh>
 
 # undef DEBUG_MESSAGE
 # define DEBUG_MESSAGE(type, from, reason, return_type) \
@@ -10,6 +11,8 @@
 
 namespace gl
 {
+
+
 	RenderPass::RenderPass()
 		: _shader(NULL)
 	{
@@ -39,142 +42,136 @@ namespace gl
 		return (*this);
 	}
 
-	RenderPass &RenderPass::setScissor(glm::ivec4 const &area)
+	void setScissor(glm::ivec4 const &area)
 	{
 		glScissor(area[0], area[1], area[2], area[3]);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setClearValue(glm::vec4 const &color, float depth, unsigned char stencil)
+	void setClearValue(glm::vec4 const &color, float depth, unsigned char stencil)
 	{
 		glClearColor(color[0], color[1], color[2], color[3]);
 		glClearDepth(depth);
 		glClearStencil(stencil);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setColorMask(GLuint index, glm::bvec4 const &color)
+	void setColorMask(GLuint index, glm::bvec4 const &color)
 	{
 		glColorMaski(index, color[0], color[1], color[2], color[3]);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setDepthMask(bool depth)
+	void setDepthMask(bool depth)
 	{
 		glDepthMask(depth);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setStencilMask(uint8_t front, uint8_t back)
+	void setStencilMask(uint8_t front, uint8_t back)
 	{
 		glStencilMaskSeparate(GL_FRONT, front);
 		glStencilMaskSeparate(GL_BACK, back);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::clear(bool color, bool depth, bool stencil)
+	void clear(bool color, bool depth, bool stencil)
 	{
 		GLenum colorBit = color ? GL_COLOR_BUFFER_BIT : 0;
 		GLenum depthBit = depth ? GL_DEPTH_BUFFER_BIT : 0;
 		GLenum stencilBit = stencil ? GL_STENCIL_BUFFER_BIT : 0;
 		glClear(colorBit | depthBit | stencilBit);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setTest(bool scissor, bool stencil, bool depth)
+	void setTest(bool scissor, bool stencil, bool depth)
 	{
 		GLint bits; glGetIntegerv(GL_STENCIL_BITS, &bits);
-		if (bits < 8) DEBUG_MESSAGE("warning", "RenderPass - setTest", "stencil < 8", *this);
+		if (bits < 8) DEBUG_MESSAGE("warning", "RenderPass - setTest", "stencil < 8", );
 		if (scissor) { glEnable(GL_SCISSOR_TEST); }
 		else { glDisable(GL_SCISSOR_TEST); }
 		if (stencil) { glEnable(GL_STENCIL_TEST); }
 		else { glDisable(GL_STENCIL_TEST); }
 		if (depth) { glEnable(GL_DEPTH_TEST); }
 		else { glDisable(GL_DEPTH_TEST); }
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setStencilFunctionFrontFace(GLenum func, int ref, uint8_t mask)
+	void setStencilFunctionFrontFace(GLenum func, int ref, uint8_t mask)
 	{
 		glStencilFuncSeparate(GL_FRONT, func, ref, mask);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setStencilOperationFrontFace(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass)
+	void setStencilOperationFrontFace(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass)
 	{
 		glStencilOpSeparate(GL_FRONT, opStencilFail, opDepthFail, opDepthPass);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setStencilFunctionBackFace(GLenum func, int ref, uint8_t mask)
+	void setStencilFunctionBackFace(GLenum func, int ref, uint8_t mask)
 	{
 		glStencilFuncSeparate(GL_BACK, func, ref, mask);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setStencilOperationBackFace(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass)
+	void setStencilOperationBackFace(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass)
 	{
 		glStencilOpSeparate(GL_BACK, opStencilFail, opDepthFail, opDepthPass);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setStencilFunction(GLenum func, int ref, uint8_t mask)
+	void setStencilFunction(GLenum func, int ref, uint8_t mask)
 	{
 		glStencilFunc(func, ref, mask);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setStencilOperation(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass)
+	void setStencilOperation(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass)
 	{
 		glStencilOp(opStencilFail, opDepthFail, opDepthPass);
-		return (*this);
-	}
-	
-	RenderPass &RenderPass::use()
-	{
-		if (!_shader)
-			DEBUG_MESSAGE("Warning", "RenderPass - use", "no shader assign on this renderPass", *this);
-		_shader->use();
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setBlend(int drawBuffer, bool state)
+	void setBlend(int drawBuffer, bool state)
 	{
 		if (state)
 			glEnablei(GL_BLEND, drawBuffer);
 		else
 			glDisablei(GL_BLEND, drawBuffer);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setBlendEquation(GLenum colorMode, GLenum alphaMode)
+	void setBlendEquation(GLenum colorMode, GLenum alphaMode)
 	{
 		glBlendEquationSeparate(colorMode, alphaMode);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setBlendEquation(GLenum mode)
+	void setBlendEquation(GLenum mode)
 	{
 		glBlendEquation(mode);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setBlendFunc(GLenum srcRGB, GLenum destRGB, GLenum srcAlpha, GLenum destAlpha)
+	void setBlendFunc(GLenum srcRGB, GLenum destRGB, GLenum srcAlpha, GLenum destAlpha)
 	{
 		glBlendFuncSeparate(srcRGB, destRGB, srcAlpha, destRGB);
-		return (*this);
 	}
-	
-	RenderPass &RenderPass::setBlendFunc(GLenum src, GLenum dest)
+
+	void setBlendFunc(GLenum src, GLenum dest)
 	{
 		glBlendFunc(src, dest);
-		return (*this);
 	}
 
-	RenderPass &RenderPass::setBlendConstant(glm::vec4 const &blendColor)
+	void setBlendConstant(glm::vec4 const &blendColor)
 	{
 		glBlendColor(blendColor[0], blendColor[1], blendColor[2], blendColor[4]);
+	}
+
+	RenderPass &RenderPass::pushSetScissorTask(glm::ivec4 const &area)
+	{
+		Command task;
+
+		task.nbrParams = 1;
+		task.params = new void *[task.nbrParams];
+		task.params[0] = new glm::ivec4;
+		memcpy(task.params[0], &area, sizeof(glm::ivec4));
+		task.func = &setScissor;
+		_tasks.push_back(task);
 		return (*this);
 	}
 
+	RenderPass &RenderPass::draw(Drawable *objectRender, size_t nbrObjectRender)
+	{
+		
+		if (!_shader)
+			DEBUG_MESSAGE("Warning", "RenderPass - use", "no shader assign on this renderPass", *this);
+		_shader->use();
+		return (*this);
+	}
 }
--- GameEngine/GameEngine/Engine/OpenGL/RenderPass.hh
+++ GameEngine/GameEngine/Engine/OpenGL/RenderPass.hh
@@ -4,10 +4,66 @@
 #include <glm/glm.hpp>
 #include <stdint.h>
 #include <tuple>
+#include <utility>
+#include <vector>
+#include <functional>
 
 namespace gl
 {
 	class Shader;
+	class Drawable;
+
+	// set buffer config
+	void setClearValue(glm::vec4 const &color, float depth, uint8_t stencil);
+	void setColorMask(GLuint index, glm::bvec4 const &color);
+	void setDepthMask(bool depth);
+	void setStencilMask(uint8_t front, uint8_t back);
+	void clear(bool color, bool depth, bool stencil);
+	void setScissor(glm::ivec4 const &area);
+	void setStencilFunctionFrontFace(GLenum func, int ref, uint8_t mask);
+	void setStencilOperationFrontFace(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass);
+	void setStencilFunctionBackFace(GLenum func, int ref, uint8_t mask);
+	void setStencilOperationBackFace(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass);
+	void setStencilFunction(GLenum func, int ref, uint8_t mask);
+	void setStencilOperation(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass);
+	void setBlend(int drawBuffer, bool state);
+	void setBlendEquation(GLenum colorMode, GLenum alphaMode);
+	void setBlendEquation(GLenum mode);
+	void setBlendFunc(GLenum srcRGB, GLenum destRGB, GLenum srcAlpha, GLenum destAlpha);
+	void setBlendFunc(GLenum src, GLenum dest);
+	void setBlendConstant(glm::vec4 const &blendColor);
+	void setTest(bool scissor, bool stencil, bool depth);
+
+	enum TypeCommand
+	{
+		CLEAR_VALUE = 0,
+		COLOR_MASK,
+		DEPTH_MASK,
+		STENCIL_MASK,
+		CLEAR,
+		SCISSOR,
+		STENCIL_FUNCTION_FRONT,
+		STENCIL_OPERATION_FRONT,
+		STENCIL_FUNCTION_BACK,
+		STENCIL_OPERATION_BACK,
+		STENCIL_FUNCTION,
+		STENCIL_OPERATION,
+		BLEND,
+		BLEND_EQUATION_S,
+		BLEND_EQUATION,
+		BLEND_FUNC_S,
+		BLEND_FUNC,
+		BLEND_CONSTANT,
+		TEST
+	};
+
+	struct Command
+	{
+		TypeCommand type;
+		int nbrParams;
+		void **params;
+		std::function<void(void **)> func;
+	};
 
 	//!\file RenderPass.hh
 	//!\author Dorian Pinaud
@@ -23,30 +79,12 @@ namespace gl
 		RenderPass(RenderPass const &copy);
 		RenderPass &operator=(RenderPass const &r);
 
-		RenderPass &setClearValue(glm::vec4 const &color, float depth, uint8_t stencil);
-		RenderPass &setColorMask(GLuint index, glm::bvec4 const &color);
-		RenderPass &setDepthMask(bool depth);
-		RenderPass &setStencilMask(uint8_t front, uint8_t back);
-		RenderPass &clear(bool color, bool depth, bool stencil);
-		RenderPass &setScissor(glm::ivec4 const &area);
-		RenderPass &setStencilFunctionFrontFace(GLenum func, int ref, uint8_t mask);
-		RenderPass &setStencilOperationFrontFace(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass);
-		RenderPass &setStencilFunctionBackFace(GLenum func, int ref, uint8_t mask);
-		RenderPass &setStencilOperationBackFace(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass);
-		RenderPass &setStencilFunction(GLenum func, int ref, uint8_t mask);
-		RenderPass &setStencilOperation(GLenum opStencilFail, GLenum opDepthFail, GLenum opDepthPass);
-		RenderPass &setBlend(int drawBuffer, bool state);
-		RenderPass &setBlendEquation(GLenum colorMode, GLenum alphaMode);
-		RenderPass &setBlendEquation(GLenum mode);
-		RenderPass &setBlendFunc(GLenum srcRGB, GLenum destRGB, GLenum srcAlpha, GLenum destAlpha);
-		RenderPass &setBlendFunc(GLenum src, GLenum dest);
-		RenderPass &setBlendConstant(glm::vec4 const &blendColor);
-
-		RenderPass &setTest(bool scissor, bool stencil, bool depth);
-		RenderPass &use();
+		RenderPass &draw(Drawable *objectRender, size_t nbrObjectRender);
+		RenderPass &pushSetScissorTask(glm::ivec4 const &area);
 
 	private:
 		Shader *_shader;
-
+		std::vector<Command> _task;
 	};
+
 }
