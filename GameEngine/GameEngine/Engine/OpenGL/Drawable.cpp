476f11c38b623aebae5ef54e182763120f5f566f
dorianpinaud <dorian.pinaud@gmail.com>
2014-07-13 23:58:58 +0200

    Drawable avance

--- GameEngine/GameEngine/Engine/OpenGL/Drawable.cpp
+++ GameEngine/GameEngine/Engine/OpenGL/Drawable.cpp
@@ -1,4 +1,6 @@
 #include <OpenGL/Drawable.hh>
+#include <OpenGL/Data.hh>
+#include <OpenGL/Pool.hh>
 
 namespace gl
 {
@@ -21,5 +23,34 @@ namespace gl
 	{
 		_model = m;
 		return (*this);
+	}
+
+	Drawable &Drawable::setGeometry(Key<Vertices> const &vertices, Key<Indices> const &indices, Key<VertexPool> const &vertexPool, Key<IndexPool> const &indexPool)
+	{
+		_vertices = vertices;
+		_indices = indices;
+		_vertexPool = vertexPool;
+		_indexPool = indexPool;
+		return (*this);
+	}
+
+	Key<IndexPool> const &Drawable::getIndexPool() const
+	{
+		return (_indexPool);
+	}
+
+	Key<VertexPool> const &Drawable::getVertexPool() const
+	{
+		return (_vertexPool);
+	}
+
+	Key<Vertices> const &Drawable::getVertices() const
+	{
+		return (_vertices);
+	}
+
+	Key<Indices> const &Drawable::getIndices() const
+	{
+		return (_indices);
 	}
 }
--- GameEngine/GameEngine/Engine/OpenGL/Drawable.hh
+++ GameEngine/GameEngine/Engine/OpenGL/Drawable.hh
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <OpenGL/Key.hh>
 
 namespace gl
 {
+	class Vertices;
+	class Indices;
+	class VertexPool;
+	class IndexPool;
+
 	//!\file Data.hh
 	//!\author Dorian Pinaud
 	//!\version v2.0
@@ -17,8 +23,18 @@ namespace gl
 
 		glm::mat4 const &getModel() const;
 		Drawable &setModel(glm::mat4 const &m);
+		Drawable &setGeometry(Key<Vertices> const &, Key<Indices> const &, Key<VertexPool> const &, Key<IndexPool> const &);
+		Key<Vertices> const &getVertices() const;
+		Key<Indices> const &getIndices() const;
+		Key<VertexPool> const &getVertexPool() const;
+		Key<IndexPool> const &getIndexPool() const;
 	private:
+
 		glm::mat4 _model;
+		Key<Vertices> _vertices;
+		Key<Indices> _indices;
+		Key<VertexPool> _vertexPool;
+		Key<IndexPool> _indexPool;
 
 	};
 }
