42d18e028de0db83a3eddfc36f0605270d0ab63b
dorianpinaud <dorian.pinaud@gmail.com>
2014-09-11 15:14:00 +0200

    replace sphere on renderOnScreen by quad form

--- GameEngine/GameEngine/Engine/Render/RenderManager.cpp
+++ GameEngine/GameEngine/Engine/Render/RenderManager.cpp
@@ -536,8 +536,8 @@ namespace gl
 		Key<RenderOnScreen> key;
 
 		createPreShaderQuad();
-		geometryManager.createSphereSimpleForm();
-		auto &element = _renderOnScreen[key] = new RenderOnScreen(geometryManager.getSimpleFormGeo(SimpleForm::SPHERE), *_preShaderQuad, geometryManager);
+		geometryManager.createQuadSimpleForm();
+		auto &element = _renderOnScreen[key] = new RenderOnScreen(geometryManager.getSimpleFormGeo(SimpleForm::QUAD), *_preShaderQuad, geometryManager);
 		element->pushInputSampler(_preShaderQuad->getSampler(0), GL_COLOR_ATTACHMENT0);
 		element->configRect(rect);
 		return (key);
