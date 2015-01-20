0d206f1b4765a2da2af345f95e3c63b5ad0a84a5
Cesar Leblic <cesar.leblic@gmail.com>
2015-01-19 22:34:50 -0500

    Old material file removed

--- GameEngine/GameEngine/Engine/Geometry/Material.cpp
+++
@@ -1,9 +0,0 @@
-#include "Material.hpp"
-
-SERIALIZATION_SERIALIZE_METHOD_DECLARATION(AGE::MaterialData, SERIALIZATION_CODE_BLOCK(ar(SERIALIZATION_CODE_BLOCKdiffuse, ambient, emissive, reflective, specular, diffuseTexPath, ambientTexPath, emissiveTexPath, reflectiveTexPath, specularTexPath, normalTexPath, bumpTexPath);))
-
-template <class Archive>
-void serialize(Archive &ar)
-{
-	
-}
