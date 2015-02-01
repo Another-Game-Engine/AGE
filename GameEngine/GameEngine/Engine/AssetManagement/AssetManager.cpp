25504fa58dcf4900c5b7e7734ba0b9c2159546d3
dorianpinaud <dorian.pinaud@gmail.com>
2015-02-01 15:39:43 +0000

    add transform set program

--- GameEngine/GameEngine/Engine/AssetManagement/AssetManager.cpp
+++ GameEngine/GameEngine/Engine/AssetManagement/AssetManager.cpp
@@ -292,6 +292,7 @@ namespace AGE
 			auto &painter = paintingManager.get_painter(mesh->painter);
 			mesh->vertices = painter->add_vertices(data.positions.size(), data.indices.size());
 			mesh->transformation = painter->get_vertices(mesh->vertices)->add_property(std::make_shared<Transformation>(Transformation(glm::mat4(1.0f))));
+			painter->get_vertices(mesh->vertices)->get_property<Transformation>(mesh->transformation)->set_program(GetRenderThread()->_pipelines[0]->get_programs());
 			auto vertices = painter->get_vertices(mesh->vertices);
 			for (auto i = 0ull; i < vertices->nbr_buffer(); ++i)
 			{
