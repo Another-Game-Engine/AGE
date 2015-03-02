5eff049f4caf4e1a350867487575fa4d03912528
Cesar Leblic <cesar.leblic@gmail.com>
2015-03-02 09:49:49 -0500

    linking error

--- GameEngine/GameEngine/Engine/Threads/RenderThread.cpp
+++ GameEngine/GameEngine/Engine/Threads/RenderThread.cpp
@@ -44,7 +44,7 @@ namespace AGE
 				msg.setValue(false);
 				return;
 			}
-			pipelines[DEFERRED] = std::make_unique<DeferredShading>(_context->getScreenSize(), paintingManager);
+			//pipelines[DEFERRED] = std::make_unique<DeferredShading>(_context->getScreenSize(), paintingManager);
 			pipelines[BASIC] = std::make_unique<BasicPipeline>(paintingManager);
 			msg.setValue(true);
 		});
