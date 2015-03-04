26c78b2e6a2045c79694f19d90137a2f7b67653b
dorianpinaud <dorian.pinaud@gmail.com>
2015-03-04 22:22:21 +0000

    deplace macro inside private method, and implement create type for array resource

--- GameEngine/GameEngine/Engine/Render/ProgramResources/Factory/UniformsFactory.cpp
+++ GameEngine/GameEngine/Engine/Render/ProgramResources/Factory/UniformsFactory.cpp
@@ -5,30 +5,14 @@
 #include <Render/ProgramResources/Types/Uniform/Sampler/Sampler2D.hh>
 
 #define LAMBDA_PROTO [this](GLint id, std::string &&name)
-
-# define DECLAR_BUILDERS \
-std::make_pair(GL_FLOAT_VEC4, LAMBDA_PROTO\
-{ \
-	return (std::make_shared<Vec4>(glm::vec4(0.0f), id, std::move(name))); \
-}), \
-std::make_pair(GL_FLOAT_MAT4, LAMBDA_PROTO\
-{ \
-	return (std::make_shared<Mat4>(glm::mat4(1.0f), id, std::move(name))); \
-}), \
-std::make_pair(GL_FLOAT, LAMBDA_PROTO\
-{ \
-	return (std::make_shared<Vec1>(0.0f, id, std::move(name))); \
-}), \
-std::make_pair(GL_SAMPLER_2D, LAMBDA_PROTO\
-{\
-	return (std::make_shared<Sampler2D>(id, std::move(name))); \
-})
+#define LAMBDA_PROTO_ARRAY [this](GLenum mode, size_t size, size_t stride, GLint id, std::string &&name)
 
 namespace AGE
 {
 
 	UniformsFactory::UniformsFactory() :
-		_blue_prints({ DECLAR_BUILDERS })
+		_blue_prints({ _create_vec4(), _create_mat4(), _create_float(), _create_sampler(),
+					_create_vec4_array(), _create_mat4_array(), _create_float_array() })
 	{
 
 	}
@@ -49,4 +33,52 @@ namespace AGE
 		return (std::shared_ptr<IProgramResources>(nullptr));
 	}
 
+	UniformsFactory::create_type_t UniformsFactory::_create_vec4() const
+	{
+		return std::make_pair(GL_FLOAT_VEC4, LAMBDA_PROTO {
+			return (std::make_shared<Vec4>(glm::vec4(0.0f), id, std::move(name)));
+		});
+	}
+
+	UniformsFactory::create_type_t UniformsFactory::_create_mat4() const
+	{
+		return std::make_pair(GL_FLOAT_MAT4, LAMBDA_PROTO {
+			return (std::make_shared<Mat4>(glm::mat4(1.0f), id, std::move(name)));
+		});
+	}
+
+	UniformsFactory::create_type_t UniformsFactory::_create_float() const
+	{
+		return std::make_pair(GL_FLOAT, LAMBDA_PROTO {
+			return (std::make_shared<Vec1>(0.0f, id, std::move(name)));
+		});
+	}
+
+	UniformsFactory::create_type_t UniformsFactory::_create_sampler() const
+	{
+		return std::make_pair(GL_SAMPLER_2D, LAMBDA_PROTO {
+			return (std::make_shared<Sampler2D>(id, std::move(name)));
+		});
+	}
+
+	UniformsFactory::create_type_array_t UniformsFactory::_create_float_array() const
+	{
+		return  std::make_pair(GL_FLOAT, LAMBDA_PROTO_ARRAY {
+			return (std::make_shared<IProgramResources>(nullptr));
+		});
+	}
+
+	UniformsFactory::create_type_array_t UniformsFactory::_create_vec4_array() const
+	{
+		return  std::make_pair(GL_FLOAT_VEC4, LAMBDA_PROTO_ARRAY{
+			return (std::make_shared<IProgramResources>(nullptr));
+		});
+	}
+
+	UniformsFactory::create_type_array_t UniformsFactory::_create_mat4_array() const
+	{
+		return  std::make_pair(GL_FLOAT_MAT4, LAMBDA_PROTO_ARRAY{
+			return (std::make_shared<IProgramResources>(nullptr));
+		});
+	}
 }
--- GameEngine/GameEngine/Engine/Render/ProgramResources/Factory/UniformsFactory.hh
+++ GameEngine/GameEngine/Engine/Render/ProgramResources/Factory/UniformsFactory.hh
@@ -14,7 +14,9 @@ namespace AGE
 	class UniformsFactory
 	{
 	public:
-
+		typedef std::pair<GLenum, std::function<std::shared_ptr<IProgramResources>(GLuint id, std::string &&name)>> create_type_t;
+		typedef std::pair<GLenum, std::function<std::shared_ptr<IProgramResources>(GLenum mode, size_t size, size_t stride, GLint id, std::string &&name)>> create_type_array_t;
+	
 	public:
 		UniformsFactory();
 		UniformsFactory(UniformsFactory const &copy) = delete;
@@ -25,9 +27,16 @@ namespace AGE
 		std::shared_ptr<IProgramResources> build_array(GLenum mode, size_t size, size_t stride, GLint id, std::string &&name);
 		
 	private:
-
+		create_type_t _create_vec4() const;
+		create_type_t _create_mat4() const;
+		create_type_t _create_float() const;
+		create_type_t _create_sampler() const;
+		create_type_array_t _create_float_array() const;
+		create_type_array_t _create_vec4_array() const;
+		create_type_array_t _create_mat4_array() const;
 
 	private:
-		std::vector<std::pair<GLenum, std::function<std::shared_ptr<IProgramResources>(GLuint id, std::string &&name)>>> _blue_prints;
+		std::vector<create_type_t> _blue_prints;
+		std::vector<create_type_array_t> _blue_prints_array;
 	};
 }
