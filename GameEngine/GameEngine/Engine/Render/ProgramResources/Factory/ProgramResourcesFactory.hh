449b72e1fe287b879b212edfc1ced41c4f20b49d
dorianpinaud <dorian.pinaud@gmail.com>
2015-03-04 21:51:30 +0000

    deplace the macro code inside private method

--- GameEngine/GameEngine/Engine/Render/ProgramResources/Factory/ProgramResourcesFactory.hh
+++ GameEngine/GameEngine/Engine/Render/ProgramResources/Factory/ProgramResourcesFactory.hh
@@ -9,12 +9,17 @@
 # include <Render/ProgramResources/Factory/UniformsFactory.hh>
 #include <Render/ProgramResources/Types/BlockResources.hh>
 
+#define LAMBDA_PROTO [this](GLint id, std::string &&name)
+
 namespace AGE
 {
 	class Program;
 
 	class ProgramResourcesFactory
 	{
+	public:
+		typedef std::pair<GLenum, std::function<std::shared_ptr<IProgramResources>(GLint, std::string &&)>> create_type_t;
+
 	public:
 		ProgramResourcesFactory(Program const &program);
 		ProgramResourcesFactory(ProgramResourcesFactory const &copy) = delete;
@@ -24,9 +29,15 @@ namespace AGE
 		std::shared_ptr<IProgramResources> build(GLenum mode, GLint id, std::string &&name);
 
 	private:
+		create_type_t _create_uniform();
+		create_type_t _create_uniform_block();
+		create_type_t _create_input();
+
+	private:
 		Program const &_program;
 		std::vector<std::pair<GLenum, std::function<std::shared_ptr<IProgramResources>(GLint, std::string &&)>>> _blue_prints;
 		UniformsFactory _uniformsFactory;
 		std::vector<std::unique_ptr<BlockResources>> _block_resources;
 	};
+
 }
