b4610be503bb5a232942ed3125e6350ebbd5c1d4
Cesar Leblic <cesar.leblic@gmail.com>
2015-04-04 13:08:58 -0400

    ImGui new presentation

--- GameEngine/GameEngine/Engine/Core/Engine.cpp
+++ GameEngine/GameEngine/Engine/Core/Engine.cpp
@@ -309,6 +309,7 @@ namespace AGE
 			_renderFpsStatitstics();
 		}
 #ifdef USE_IMGUI
+		ImGui::ShowTestWindow();
 		ImGui::Render();
 #endif
 		GetPrepareThread()->getQueue()->emplaceCommand<Commands::ToRender::Flush>();
