028c755f13775cd24548763a3f94209c5511816f
dorianpinaud <dorian.pinaud@gmail.com>
2015-04-12 20:33:54 +0100

    Name Creation

--- GameEngine/Tools/AssetsEditor/Scenes/MaterialEditorScene.cpp
+++ GameEngine/Tools/AssetsEditor/Scenes/MaterialEditorScene.cpp
@@ -1,6 +1,13 @@
 #include "MaterialEditorScene.hh"
 #include <imgui/imgui.h>
 #include <Utils/OpenGL.hh>
+#include <AssetManagement\Data\MaterialData.hh>
+#include <Convertor/MaterialConvertor.hpp>
+#include <Utils/Directory.hpp>
+#include <Utils/Path.hpp>
+#include <EditorConfiguration.hpp>
+
+#define NAME_LENGTH 50
 
 namespace AGE
 {
@@ -24,6 +31,16 @@ namespace AGE
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui::BeginChild("Material editor", ImVec2(0, 0), true);
+		char buffer[NAME_LENGTH];
+		memset(buffer, 0, NAME_LENGTH);
+		auto materialSet = std::make_shared<MaterialDataSet>();
+		ImGui::InputText("Material Name: ", buffer, NAME_LENGTH);
+		std::string name(buffer);
+		if (ImGui::Button("cook")) {
+			std::string const currentDir = Directory::GetCurrentDirectory();
+			const std::string absPath = Path::AbsoluteName(currentDir.c_str(), WE::EditorConfiguration::GetCookedDirectory().c_str());
+			auto fileName = absPath + name + ".mage";
+		}
 		return true;
 	}
 
