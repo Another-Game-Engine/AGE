36372e3dda5fa21bd895f5692fc241ba2f9882ca
Lauralie <laureline.patoz@gmail.com>
2015-06-02 17:13:54 +0100

    Remove FMOD references
    from the Audio code

--- GameEngine/GameEngine/Engine/Audio/AudioManager.hh
+++ GameEngine/GameEngine/Engine/Audio/AudioManager.hh
@@ -1,13 +1,10 @@
 #pragma once
 
-#include <fmod.hpp>
-#include <Audio/FmodError.hpp>
 #include <Audio/ChannelGroupType.hpp>
 #include <Utils/Dependency.hpp>
 #include <memory>
 #include <map>
 #include <iostream>
-#include <Audio/Audio.hh>
 
 class AudioManager : public std::enable_shared_from_this<AudioManager>, public Dependency<AudioManager>
 {
@@ -16,15 +13,16 @@ public:
 	virtual ~AudioManager();
 	bool init();
 	void update();
-	std::shared_ptr<Audio> loadSound(const OldFile &file, Audio::AudioSpatialType spacialType, const std::string &name = "");
+	/*std::shared_ptr<Audio> loadSound(const OldFile &file, Audio::AudioSpatialType spacialType, const std::string &name = "");
 	std::shared_ptr<Audio> loadStream(const OldFile &file, Audio::AudioSpatialType spacialType, const std::string &name = "");
 	FMOD::System *getSystem();
 	FMOD::ChannelGroup* getChannelGroup(ChannelGroupType type);
-	inline std::shared_ptr<Audio> getAudio(const std::string &name){ auto r = _audios.find(name); if (r == std::end(_audios)) return nullptr; return r->second; }
+	inline std::shared_ptr<Audio> getAudio(const std::string &name){ auto r = _audios.find(name); if (r == std::end(_audios)) return nullptr; return r->second; }*/
 private:
+	/*
 	FMOD::System *_system;
 	FMOD_SPEAKERMODE _speakerMode;
 	FMOD_CAPS _caps;
 	std::map<ChannelGroupType, FMOD::ChannelGroup*> _channelGroups;
-	std::map<std::string, std::shared_ptr<Audio>> _audios;
+	std::map<std::string, std::shared_ptr<Audio>> _audios;*/
 };
--- GameEngine/GameEngine/Engine/Audio/FmodError.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <fmod_errors.h>
-#include <iostream>
-
-bool fmodError(FMOD_RESULT result);
