8e8e373b7f851bc52b98d7894e2a4d3f3aed2fb8
Paul Baron <paulbaron7993@gmail.com>
2015-08-15 11:50:41 +0100

    Start character controller, I will finish it on another branch when @herpec-j will push is version

---
+++ GameEngine/GameEngine/CharacterControllerSystem.cpp
@@ -0,0 +1,29 @@
+#include "CharacterControllerSystem.hh"
+
+namespace AGE
+{
+	CharacterControllerSystem::CharacterControllerSystem(AScene *scene)
+	{
+	}
+
+	bool CharacterControllerSystem::initialize()
+	{
+
+	}
+
+	void CharacterControllerSystem::updateBegin(float time)
+	{
+
+	}
+
+	void CharacterControllerSystem::mainUpdate(float time)
+	{
+
+	}
+
+	void CharacterControllerSystem::updateEnd(float time)
+	{
+
+	}
+
+}
