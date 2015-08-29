0409ff8ca90ce3d89f7f01792607f71093b6d927
Jonathan Herpêche <jonathan.herpeche+github@gmail.com>
2015-08-29 17:18:01 +0200

    Compilation fixed

--- GameEngine/EngineCoreTest/EngineCoreTestConfiguration.hpp
+++ GameEngine/EngineCoreTest/EngineCoreTestConfiguration.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <Utils/FileSystemHelpers.hpp>
+#include <FileUtils/FileUtils/FileSystemHelpers.hpp>
 #include <Utils/Serialization/SerializationArchives.hpp>
 #include <fstream>
 #include <cereal/types/string.hpp>
@@ -59,7 +59,7 @@ namespace AGE
 		{
 			if (_configurations == nullptr)
 			{
-				if (!AGE::FileSystemHelpers::AgeExists("EngineCoreTestConfiguration.json"))
+				if (!FileUtils::AgeExists("EngineCoreTestConfiguration.json"))
 				{
 					_configurations = std::make_shared<ECTConfigurations>();
 					saveConfigurations();
